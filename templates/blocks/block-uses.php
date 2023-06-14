174c19f9ee617a75d5f4e4b04c413e18bfcd33c5
MattKCyr <matt@mspacecreative.com>
2023-06-13 21:02:55 -0300

    c

--- templates/blocks/block-uses.php
+++ templates/blocks/block-uses.php
@@ -3,7 +3,7 @@ $uses = get_field('traditional_uses', get_the_ID());
 if ( $uses ) {
     echo 
     '<div class="top-margin-40 has-medium-font-size">
-        <h3 class="has-medium-font-size">' . esc_html('Traditional Uses') . '</h3>';
+        <h2 class="has-large-font-size">' . esc_html('Traditional Uses') . '</h2>';
     echo
         '<div class="notes">' . $uses . '</div>
     </div>';
