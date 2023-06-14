d02a73d6badba7fb9f232e8c9ebd00378651524b
MattKCyr <matt@mspacecreative.com>
2023-06-14 07:11:46 -0300

    c

--- templates/blocks/block-notes.php
+++ templates/blocks/block-notes.php
@@ -7,7 +7,7 @@ if ( $notes ) {
     echo 
     '
     <div class="top-margin-40 has-medium-font-size">
-    <h2 class="has-medium-font-size">' . esc_html($postType->labels->singular_name) . ' ' . esc_html('Notes') . '</h2>';
+    <h2 class="has-large-font-size">' . esc_html($postType->labels->singular_name) . ' ' . esc_html('Notes') . '</h2>';
     }
     echo
     '<div class="notes">' . $notes . '</div>
