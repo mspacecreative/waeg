bc90e78a0e72d7a0716fb713900e52dc743a24a2
MattKCyr <matt@mspacecreative.com>
2023-06-13 21:37:29 -0300

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -3,6 +3,6 @@ $botanical_names = get_field('botanical_names', get_the_ID());
 if ( $botanical_names ) {
     echo
     '<div class="botanical-names bottom-margin-40">
-        <h2 class="has-medium-font-size"><strong>Botanical name(s):</strong> ' . $botanical_names . '</h2>
+        <h2 class="has-medium-font-size" style="margin-top: 0;"><strong>Botanical name(s):</strong> ' . $botanical_names . '</h2>
     </div>';
 }
