071bbb3be344fb25929faef3f0237deea8d29453
MattKCyr <matt@mspacecreative.com>
2023-06-19 15:22:33 -0300

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -1,8 +1,14 @@
 <?php
-$botanical_names = get_field('botanical_names', get_the_ID());
-if ( $botanical_names ) {
-    echo
-    '<div class="botanical-names bottom-margin-40">
-        <h2 class="has-medium-font-size"><strong>Botanical name(s):</strong> ' . $botanical_names . '</h2>
-    </div>';
+if (have_rows('botanical_names', get_the_ID())) {
+    while (have_rows('botanical_names', get_the_ID())) {
+        the_row();
+        $botanical_names = get_sub_field('botanical_names');
+        $designation = get_sub_field('designation') ? get_field('designation') : '';
+        if ( $botanical_names ) {
+            echo
+            '<div class="botanical-names bottom-margin-40">
+                <h2 class="has-medium-font-size"><strong>Botanical name(s):</strong> ' . '<span style="font-style: italic;">' . $botanical_names . '</span>' . $designation . '</h2>
+            </div>';
+        }
+    }
 }
