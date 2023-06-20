92229acb9bc97fa29a20f04cfd22e7982a3172c5
MattKCyr <matt@mspacecreative.com>
2023-06-20 11:31:24 -0300

    Added tinymce

---
+++ functions/tinymce.php
@@ -0,0 +1,28 @@
+<?php
+
+// CUSTOM STYLES ADDED TO FORMATS DROPDOWN
+function my_custom_styles( $init_array ) {  
+
+    $style_formats = array(  
+        
+        array(  
+            'title' => 'Normal Text Style',  
+            'inline' => 'span',  
+            'classes' => 'normal-text',
+            'wrapper' => true,
+        ),
+    );  
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );  
+    
+    return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_custom_styles' );
+
+// function custom_editor_styles() {
+// 	add_editor_style('assets/css/editor-styles.css');
+// }
+
+// add_action('init', 'custom_editor_styles');
