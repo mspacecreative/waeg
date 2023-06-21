69d113d5848f3346387fe59240271a16f4b3956d
MattKCyr <matt@mspacecreative.com>
2023-06-21 08:51:23 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -3,7 +3,7 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
     echo 
     '
     <div class="boxed bg--light-gray">
-        <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html_e('Traditional Names', 'waeg') . '</h2>
+        <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html__('Traditional Names', 'waeg') . '</h2>
         <ul class="no-bullets no-bottom-margin">';
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
