55f4051a20d5d0fa69f889abd8fef10079f9ed5f
MattKCyr <matt@mspacecreative.com>
2023-06-23 16:24:35 -0300

    c

--- functions/analytics.php
+++ functions/analytics.php
@@ -3,7 +3,7 @@ function google_analytics() {
     echo
     '<script id="google_gtagjs-js-after">
     window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
-    gtag('set', 'linker', {"domains":["ethnobotanygarden.com"]} );
+    gtag("set", "linker", {"domains":["ethnobotanygarden.com"]} );
     gtag("js", new Date());
     gtag("set", "developer_id.dZTNiMT", true);
     gtag("config", "UA-3578250-10", {"anonymize_ip":true});
