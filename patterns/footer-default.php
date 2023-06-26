9337d536274a5b08408aacbe2d7c51926ee29c07
MattKCyr <matt@mspacecreative.com>
2023-06-26 14:40:37 -0300

    c

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -6,11 +6,15 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);">
-		<?php echo esc_html_x('&copy; ', 'waeg'), date('Y '), get_bloginfo('title'), esc_html_x('. All rights reserved.', 'waeg' ); ?>
-	<!-- /wp:group -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"align":"right"} -->
+	<p class="has-text-align-right">
+	<?php
+	printf(
+		esc_html_e('&copy; '), esc_html_e(date('Y ')), esc_html_e(get_bloginfo('title', 'waeg')), esc_html_e('. All rights reserved.', 'waeg')
+	)
+	?>
+	</p>
+	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
