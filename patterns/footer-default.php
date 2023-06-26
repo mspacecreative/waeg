64010a9579d98136a8d736ab264671e0a60da51a
MattKCyr <matt@mspacecreative.com>
2023-06-26 12:35:25 -0300

    c

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -10,16 +10,7 @@
 <div class="wp-block-group">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);">
-			<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
-			<div class="wp-block-columns" style="margin-bottom:var(--wp--preset--spacing--40)">
-				<!-- wp:column -->
-				<div class="wp-block-column">
-				<?php echo esc_html_x('&copy; ', 'waeg'), date('Y '), get_bloginfo('title'), esc_html_x('. All rights reserved.', 'waeg' ); ?>
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
-	</div>
+		<?php echo esc_html_x('&copy; ', 'waeg'), date('Y '), get_bloginfo('title'), esc_html_x('. All rights reserved.', 'waeg' ); ?>
 	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
