dcb4ed5fa7ff97251927a8d05f36420475a003c8
MattKCyr <matt@mspacecreative.com>
2023-06-26 11:27:02 -0300

    c

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -9,8 +9,8 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":1} /-->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);">
+		<!-- wp:site-title {"level":0} /-->
 	</div>
 	<!-- /wp:group -->
 </div>
