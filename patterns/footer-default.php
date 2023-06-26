0b1a0ce15bae9f4e9ab0379e7460c1dcf2729837
MattKCyr <matt@mspacecreative.com>
2023-06-26 10:24:48 -0300

    c

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -10,16 +10,7 @@
 <div class="wp-block-group">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":0} /-->
-		<!-- wp:paragraph {"align":"right"} -->
-		<p class="has-text-align-right">
-		<?php
-		printf(
-			esc_html__( '&copy; ', 'waeg' ), date('Y '), bloginfo('name'), esc_html__('. All rights reserved.')
-		)
-		?>
-		</p>
-		<!-- /wp:paragraph -->
+		<!-- wp:site-title {"level":1} /-->
 	</div>
 	<!-- /wp:group -->
 </div>
