e19ce7914c5ea70331421917e5c53fbbceb5607b
MattKCyr <matt@mspacecreative.com>
2023-06-27 14:33:53 -0300

    c

--- templates/blocks/block-features.php
+++ templates/blocks/block-features.php
@@ -5,7 +5,7 @@ if (have_rows('features', get_the_ID())) {
     while (have_rows('features', get_the_ID())) {
         the_row();
         $feature_content = get_sub_field('feature_content');
-        $feature_icon = get_sub_field('feature_icon') ? '<img class="full-width feature__icon" src="' . esc_url($feature_icon['url']) . '" alt="' . esc_attr($feature_icon['alt']) . '" />' : '';
+        $feature_icon = get_sub_field('feature_icon') ? '<img class="full-width feature__icon" src="' . esc_url(get_sub_field('feature_icon')['url']) . '" alt="' . esc_attr(get_sub_field('feature_icon')['alt']) . '" />' : '';
         echo
         '<li class="feature">'
             . $feature_icon .
