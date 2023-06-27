6a77eb89f6e4b8395ef9a29707bbddff726f9396
MattKCyr <matt@mspacecreative.com>
2023-06-27 10:18:49 -0300

    Activate swiper for habitat

--- functions/styles-scripts.php
+++ functions/styles-scripts.php
@@ -5,7 +5,7 @@ function waeg_styles() {
         wp_register_style( 'main', get_template_directory_uri() . '/assets/css/styles.css?ver=' . $rand_num, array(), null );
 	    wp_enqueue_style( 'main' );
 
-    if (is_singular('plant')) {
+    if (is_singular(array('plant', 'habitat')) {
         // SWIPER CSS
         wp_register_style( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', array(), null );
         wp_enqueue_style( 'swiper' );
@@ -21,11 +21,11 @@ function waeg_styles() {
         // LIGHTBOX JS
         wp_register_script( 'lightbox', get_template_directory_uri() . '/assets/js/lightbox.js', array(), null, true );
         wp_enqueue_script( 'lightbox' );
-
-        // FONTAWESOME
-        wp_register_style( 'fontawesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), null );
-	    wp_enqueue_style( 'fontawesome' );
     }
+
+    // FONTAWESOME
+    wp_register_style( 'fontawesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), null );
+    wp_enqueue_style( 'fontawesome' );
 
     wp_register_script( 'main', get_template_directory_uri() . '/assets/js/main.js?ver=' . $rand_num, array(), null, true );
 	wp_enqueue_script( 'main' );
