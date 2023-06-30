fe009baf68ed814674e895cb1720d26a520dac0e
MattKCyr <matt@mspacecreative.com>
2023-06-30 15:47:54 -0300

    c

--- templates/blocks/block-sitemap.php
+++ templates/blocks/block-sitemap.php
@@ -1,16 +1,21 @@
-<h2 id="posts">Plants</h2>
+
+<?php
+$posttype = get_field('post_type');
+?>
+<h3>Plants</h2>
 <ul>
 <?php
     $not_in = array(); //to avoid naming the same post over and over
     //get top level terms
+
     $Parent_terms = get_terms( 'species', array('orderby' => 'name','parent' => 0));
     foreach ($Parent_terms as $term) {
-        echo "<li><h3>".$term->name."</h3>";
+        echo "<li><a href=" . esc_url(get_term_link($term)) . "><strong>" . $term->name . "</strong></a>";
         echo "<ul>";
         //get all posts that are only listed in top level term
         $args = array(
             'post__not_in' => $not_in,
-            'post_type' => 'plant',
+            'post_type' => $posttype,
             'posts_per_page' => -1,
             'tax_query' => array(
                 array(
@@ -25,7 +30,6 @@
             $new->the_post();
             $not_in[] = $post->ID;
             echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-            //here you should check for children posts and output them 
         }
         echo "</ul>";
         echo "</li>";
