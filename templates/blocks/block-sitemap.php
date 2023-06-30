6430d9a8c99f076076ab9b572cd57bd18431952b
MattKCyr <matt@mspacecreative.com>
2023-06-30 15:35:52 -0300

    c

--- templates/blocks/block-sitemap.php
+++ templates/blocks/block-sitemap.php
@@ -7,33 +7,6 @@
     foreach ($Parent_terms as $term) {
         echo "<li><h3>".$term->name."</h3>";
         echo "<ul>";
-        //get all children of each term
-        $child_terms = get_terms( 'species', array('orderby' => 'name','parent' => $term->ID));
-        foreach ((array)$child_terms as $t){
-            echo "<li><h4>".$t->name."</h4>";
-            echo "<ul>";
-            $args = array(
-                'post__not_in' => $not_in,
-                'post_type' => 'plant',
-                'posts_per_page' => -1,
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'species',
-                        'field' => 'slug',
-                        'terms' => $t->slug
-                    )
-                )
-            );
-            $new = new WP_Query($args);
-            while ($new->have_posts()) {
-                $new->the_post();
-                $not_in[] = $post->ID;
-                echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-                //here you should check for children posts and output them 
-            }
-            echo "</ul>";
-            echo "</li>";
-        }
         //get all posts that are only listed in top level term
         $args = array(
             'post__not_in' => $not_in,
