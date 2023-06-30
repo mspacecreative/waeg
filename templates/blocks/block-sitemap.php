47734b3da53666330ddee80b329e97601f9ff9f1
MattKCyr <matt@mspacecreative.com>
2023-06-30 16:23:13 -0300

    c

--- templates/blocks/block-sitemap.php
+++ templates/blocks/block-sitemap.php
@@ -5,7 +5,7 @@ $label = $posttype['label'];
 $value = $posttype['value'];
 ?>
 <h3><?php echo $label ?></h2>
-<ul class='margin-block-end: var(--wp--preset--spacing--30)'>
+<ul style='margin-block-end: var(--wp--preset--spacing--30)'>
 <?php
     $not_in = array(); //to avoid naming the same post over and over
     //get top level terms
