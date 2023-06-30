c8afafa294a53ee33789536f2cb0437b089b314b
MattKCyr <matt@mspacecreative.com>
2023-06-30 12:29:07 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -4,12 +4,12 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
     '
     <div class="boxed bg--light-gray">
         <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html__('Traditional Names', 'waeg') . '</h2>
+        <strong>' . esc_html__('Gwich&#8217;in') . '</strong>
         <ul class="no-bullets no-bottom-margin">';
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if ( $names = get_row() ) {
             $i = 0;
-            echo '<strong>' . esc_html__('Gwich&#8217;in') . '</strong>';
             foreach ($names as $key => $value) {
                 if (!empty($value) ) { 
                 $field = get_sub_field_object( $key );
