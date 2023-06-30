7a65042613f1e9e47be9be60560baefbdabeb117
MattKCyr <matt@mspacecreative.com>
2023-06-30 13:36:30 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -5,7 +5,7 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
     <div class="boxed bg--light-gray">
         <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html__('Traditional Names', 'waeg') . '</h2>
         <strong>' . esc_html_x('Gwich&#8217;in', 'waeg') . '</strong>
-        <ul class="no-bullets no-bottom-margin">';
+        <ul class="no-bullets">';
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if ( $names = get_row() ) {
@@ -27,14 +27,16 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if ( $names = get_row() ) {
-            foreach ($names as $key => $value) {
-                if (!empty($value) ) { 
-                $field = get_sub_field_object( $key );
-                echo
-                '<li class="has-medium-font-size">' . '<strong>' . $field['label'] . ':</strong> ' . $value . '</li>';
+                $i = 0;
+                $arr = array();
+                foreach ($names as $key => $value) {
+                    if ($i > 2) continue;
+                    if (!empty($value) ) { 
+                    $field = get_sub_field_object( $key );
+                    echo
+                    '<li class="has-medium-font-size">' . '<strong>' . $field['label'] . ':</strong> ' . $value . '</li>';
+                    }
                 }
-                if ($field > 2) continue;
-            }
             }
         }
         echo 
