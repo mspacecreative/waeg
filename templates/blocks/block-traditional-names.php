892a22e505cef5789d1ab012605dfde2f403bf9e
MattKCyr <matt@mspacecreative.com>
2023-06-30 12:22:46 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -9,12 +9,12 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if ( $names = get_row() ) {
             foreach ($names as $key => $value) {
+                if (++$i == 2) break;
                 if (!empty($value) ) { 
                 $i = 0;
                 $field = get_sub_field_object( $key );
                 echo
                 '<li class="has-medium-font-size">' . '<strong>' . $field['label'] . ':</strong> ' . $value . '</li>';
-                if (++$i == 2) break;
                 }
             }
             }
