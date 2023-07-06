fdff67b30a67b95caae22755b1aa27dce2e9a78e
MattKCyr <matt@mspacecreative.com>
2023-07-06 17:11:53 -0300

    c

--- templates/blocks/block-other-species.php
+++ templates/blocks/block-other-species.php
@@ -1,6 +1,20 @@
 <?php
 $tax = 'species';
-
+$cols = get_field('columns');
+switch($cols) {
+    case '1':
+        $cols = '1';
+        break;
+    case '2':
+        $cols = '2';
+        break;
+    case '3':
+        $cols = '3';
+        break;
+    default:
+        $cols = '3';
+        break;
+}
 $terms = get_terms( $tax, $args = array(
   'hide_empty' => true,
 ));
