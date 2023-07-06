9b929eecd2e5ffb1f856e6c684dfed952a3b7da1
MattKCyr <matt@mspacecreative.com>
2023-07-06 15:58:44 -0300

    Updated habitat block options and logic

--- templates/blocks/block-habitats.php
+++ templates/blocks/block-habitats.php
@@ -1,6 +1,24 @@
 <?php
+$posttype = get_field('post_type');
+$cols = get_field('columns');
+$label = $posttype['label'];
+$value = $posttype['value'];
+switch($cols) {
+    case '1':
+        $cols = '1';
+        break;
+    case '2':
+        $cols = '2';
+        break;
+    case '3':
+        $cols = '3';
+        break;
+    default:
+        $cols = '3';
+        break;
+}
 $args = array(
-    'post_type' => 'habitat',
+    'post_type' => $value,
     'post__not_in' => array(get_the_ID()),
 );
 
@@ -8,7 +26,8 @@ $loop = new WP_Query($args);
 
 if ($loop->have_posts()) {
     echo
-    '<ul class="is-flex-container columns-3 wp-block-post-template wp-block-other-habitats">';
+    '<h2 class="wp-block-heading has-large-font-size">' . esc_html_x('Other ', 'waeg') . $label . '</h2>
+    <ul class="is-flex-container columns-' . $cols . ' wp-block-post-template wp-block-other-habitats">';
     while ($loop->have_posts()) {
         $loop->the_post();
         $featured_img = get_the_post_thumbnail(get_the_ID(), 'swiper-thumb');
