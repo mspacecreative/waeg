fb8a0c533e12fb3968815c602d735a361d600d9d
MattKCyr <matt@mspacecreative.com>
2023-07-06 12:02:08 -0300

    c

--- templates/blocks/block-term.php
+++ templates/blocks/block-term.php
@@ -1,18 +1,30 @@
 <?php
 $query = new WP_Query( array(
-    'post_type' => 'plant',          // name of post type.
+    'post_type' => 'plant',
     'tax_query' => array(
         array(
-            'taxonomy' => 'species',   // taxonomy name
-            'field' => 'term_id',           // term_id, slug or name
-            'terms' => get_queried_object()->term_id        // term id, term slug or term name
+            'taxonomy' => 'species',
+            'field' => 'term_id',
+            'terms' => get_queried_object()->term_id
         )
     )
 ) ); ?>
-<ul>
+<ul class="is-flex-container columns-3 wp-block-post-template">
 <?php while ( $query->have_posts() ) : $query->the_post();
-    echo 
-    '<li><a href=' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
+    $loop->the_post();
+    $featured_img = get_the_post_thumbnail(get_the_ID(), 'swiper-thumb');
+    $title = get_the_title();
+    $permalink = get_the_permalink();
+
+    echo
+    '<li class="wp-block-post">
+        <figure class="wp-block-post-featured-image">
+            <a href="' . $permalink . '">'
+                . $featured_img . 
+            '</a>
+        </figure>
+        <h3 class="wp-block-post-title has-large-font-size">' . esc_html__($title) . '</h3>
+    </li>';
 endwhile; ?>
 </ul>
 
