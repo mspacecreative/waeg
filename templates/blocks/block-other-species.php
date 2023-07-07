b6226118842bdbb209232aa729e9e194472fa085
MattKCyr <matt@mspacecreative.com>
2023-07-07 13:38:59 -0300

    c

--- templates/blocks/block-other-species.php
+++ templates/blocks/block-other-species.php
@@ -1,8 +1,11 @@
 <?php
 $berries_img = get_field('berries', 'option');
 $flowers_img = get_field('flowers', 'option');
+$shrubs_img = get_field('shrubs', 'option');
+$size = 'swiper-thumb';
 $tax = 'species';
 $cols = get_field('columns');
+$featured_img = $shrubs_img ? wp_get_attachment_image( $shrubs_img, $size ) : '';
 switch($cols) {
     case '1':
         $cols = '1';
@@ -24,7 +27,7 @@ $terms = get_terms( $tax, array(
 
 echo
 '<h2 class="wp-block-heading has-large-font-size">' . esc_html_x('Other Species', 'waeg') . '</h2>
-<ul class="is-flex-container columns-' . $cols . ' wp-block-post-template-container wp-block-post-template wp-block-other-habitats">';
+<ul class="is-flex-container columns-' . $cols . ' wp-block-post-template-container wp-block-post-template wp-block-other-habitats">' . wp_get_attachment_image( $shrubs_img, $size );
 
 foreach( $terms as $term ) {
 
@@ -32,16 +35,14 @@ foreach( $terms as $term ) {
 
     if( $term->count > 0 ) {
         
-        $term = get_queried_object();
-        $size = 'swiper-thumb';
-        $featured_img = get_field('shrubs_img', $term) ? wp_get_attachment_image( get_field('shrubs_img', $term), $size ) : '';
+        // $featured_img = $term->slug == 'shrubs' ? wp_get_attachment_image( $shrubs_img, $size ) : $term->slug == 'flowers' ? wp_get_attachment_image( $flowers_img, $size ) : $term->slug == 'berries' ? wp_get_attachment_image( $berries_img, $size ) : '';
         // $permalink = get_the_permalink();
 
         echo
         '<li class="wp-block-post">
             <figure class="wp-block-post-featured-image">
                 <a href="' . esc_url( $term_link ) . '">'
-                    . $featured_img . 
+                    . wp_get_attachment_image( $shrubs_img, $size ) . 
                 '</a>
             </figure>
             <h3 class="wp-block-post-title has-large-font-size">' . $term->name . '</h3>
