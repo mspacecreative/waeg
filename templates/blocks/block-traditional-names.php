5c1c69597ad6cbf282a3335ffa606b9a4a52e53d
MattKCyr <matt@mspacecreative.com>
2023-07-10 14:54:54 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -36,7 +36,29 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if ( $names = get_row() ) {
-                foreach (array_slice($names, 2) as $key => $value) {
+                foreach (array_slice($names,2,3) as $key => $value) {
+                    if (!empty($value) ) { 
+                    $field = get_sub_field_object( $key );
+                    echo
+                    '<li class="has-medium-font-size">' . '<strong>' . $field['label'] . ':</strong> ' . $value . '</li>';
+                    }
+                }
+            }
+        }
+        echo 
+        '</ul>';
+        while (have_rows('traditional_names', get_the_ID()) ) {
+        the_row();
+        if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
+        echo
+        '<strong class="underlined-heading font-weight-normal font-size-22">&nbsp;</strong>';
+        }
+        echo
+        '<ul class="no-bullets no-bottom-margin">';
+        while ( have_rows('traditional_names', get_the_ID()) ) {
+            the_row();
+            if ( $names = get_row() ) {
+                foreach (array_slice($names,4) as $key => $value) {
                     if (!empty($value) ) { 
                     $field = get_sub_field_object( $key );
                     echo
