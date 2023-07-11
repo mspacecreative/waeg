62420a82104482be56dbb32b1fcae6f523059c27
MattKCyr <matt@mspacecreative.com>
2023-07-11 10:23:18 -0300

    C

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -1,8 +1,7 @@
 <?php
 if ( have_rows('traditional_names', get_the_ID()) ) {
     echo 
-    '
-    <div class="boxed bg--light-gray">
+    '<div class="boxed bg--light-gray">
         <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html__('Traditional Names', 'waeg') . '</h2>';
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
@@ -44,14 +43,13 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
             //         }
             //     }
             // }
-        }
         // echo 
         // '</ul>';
         while (have_rows('traditional_names', get_the_ID()) ) {
-        the_row();
-        if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
-        echo
-        '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('Inuvialuktun', 'waeg') . '</strong>';
+            the_row();
+            if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
+            echo
+            '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('Inuvialuktun', 'waeg') . '</strong>';
         }
         echo
         '<ul class="no-bullets">';
@@ -70,10 +68,10 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
         echo 
         '</ul>';
         while (have_rows('traditional_names', get_the_ID()) ) {
-        the_row();
-        if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
-        echo
-        '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('French and English', 'waeg') . '</strong>';
+            the_row();
+            if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
+            echo
+            '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('French and English', 'waeg') . '</strong>';
         }
         echo
         '<ul class="no-bullets no-bottom-margin">';
