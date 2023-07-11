6417d3cb6a68d65ed14e1a08e2d63dce1a828be8
MattKCyr <matt@mspacecreative.com>
2023-07-11 10:06:17 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -6,13 +6,34 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
         <h2 class="no-top-margin has-large-font-size bottom-margin-1em bg--light-green color--white">' . esc_html__('Traditional Names', 'waeg') . '</h2>';
         while (have_rows('traditional_names', get_the_ID()) ) {
         the_row();
-        if (!empty(get_sub_field('gwichya_name') || get_sub_field('teetlit_name')))
+        if (!empty(get_sub_field('gwichya_name')))
         echo '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('Gwich&#8217;in', 'waeg') . '</strong>';
         }
-        echo
-        '<ul class="no-bullets">';
+        // echo
+        // '<ul class="no-bullets">';
         while ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
+            if (have_rows('gwichya_dialect')) {
+                echo
+                '<ul class="no-bullets">';
+                while (have_rows('gwichya_dialect')) {
+                    the_row();
+                    $name = get_sub_field('gwichya_name');
+                    $audiofile = get_sub_field('audio_file');
+                    if ($name && $audio) {
+                        echo
+                        '<li class="has-medium-font-size position-relative">' . '<strong>' . esc_html_x('Gwichya', 'waeg') . ':</strong> ' . $name . '
+                            <button class="audio-trigger"><i class="fa fa-volume-up"></i></button>
+                            <audio src="' . $audiofile . '"></audio>
+                        </li>';
+                    } elseif ($name) {
+                        echo
+                        '<li class="has-medium-font-size position-relative">' . '<strong>' . esc_html_x('Gwichya', 'waeg') . ':</strong> ' . $name . '</li>';
+                    }
+                }
+                echo 
+                '</ul>';
+            }
             if ( $names = get_row() ) {
                 foreach (array_slice($names,0,2) as $key => $value) {
                     if (!empty($value) ) { 
@@ -23,8 +44,8 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
                 }
             }
         }
-        echo 
-        '</ul>';
+        // echo 
+        // '</ul>';
         while (have_rows('traditional_names', get_the_ID()) ) {
         the_row();
         if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
