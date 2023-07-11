9deeec0c438054d7c14c77665e0418cc5813e897
MattKCyr <matt@mspacecreative.com>
2023-07-11 09:11:43 -0300

    c

--- functions/audio-loop.php
+++ functions/audio-loop.php
@@ -4,10 +4,10 @@ function audio_loop() {
         while ( have_rows('gwichya_dialect', get_the_ID()) ) {
             the_row();
             $audiofile = get_sub_field('audio_file');
-            $i = 0;
+            $i = 1;
             echo
             '<button class="audio-trigger"><i class="fa fa-volume-up"></i></button>
-            <audio id=audio-' . $i++ . '" src="' . $audiofile . '"></audio>';
+            <audio id="audio-' . $i++ . '" src="' . $audiofile . '"></audio>';
         }
     }
 }
