6ad6bc2ef25075aa8b43dc90d6950a54b41ba0ef
MattKCyr <matt@mspacecreative.com>
2023-07-11 09:24:34 -0300

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ for (i = 0; i < audioFile.length; i++) {
 for (i = 0; i < audioTrigger.length; i++) {
   audioTrigger[i].addEventListener("click", (e) => {
     e.preventDefault();
-    this.nextSibling(audioFile).play();
+    this.nextElementSibling().play();
   });
 }
 
