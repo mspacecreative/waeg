26997415dd505b563bfe91985f3fae591bfccb54
MattKCyr <matt@mspacecreative.com>
2023-07-11 08:55:42 -0300

    C

--- functions/audio-loop.php
+++ functions/audio-loop.php
@@ -1,7 +1,7 @@
 <?php
 function audio_loop() {
-    if ( have_rows('gwichya_dialect') ) {
-        while ( have_rows('gwichya_dialect') ) {
+    if ( have_rows('gwichya_dialect', get_the_ID()) ) {
+        while ( have_rows('gwichya_dialect', get_the_ID()) ) {
             the_row();
             $audiofile = get_sub_field('audio_file');
             echo
