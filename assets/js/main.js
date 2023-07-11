c6a3f949c30aef2d6e3df132665f6998f8cacc76
MattKCyr <matt@mspacecreative.com>
2023-07-11 09:20:24 -0300

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // AUDIO TRIGGER
 let audioTrigger = document.querySelectorAll(".audio-trigger");
 const audioFile = document.querySelectorAll("audio");
-const num = 0;
+let num = 0;
 for (i = 0; i < audioFile.length; i++) {
   audioFile[i].setAttribute("id", "audio-" + num++);
 }
