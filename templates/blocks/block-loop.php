43f094bec52a9655152ccc748f750a5abae6c88a
MattKCyr <matt@mspacecreative.com>
2023-07-19 14:07:25 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -1,11 +1,34 @@
 <?php
 $posttype = get_field('post_type');
+$cols = get_field('columns');
+
+switch($cols) {
+    case '1':
+        $cols = '1';
+        break;
+    case '2':
+        $cols = '2';
+        break;
+    case '3':
+        $cols = '3';
+        break;
+    default:
+        $cols = '3';
+        break;
+}
+
+// CUSTOM CLASS	
+$className = '';
+if( !empty($block['className']) ) {
+	$className .= ' ' . $block['className'];
+}
+
 $loop = new WP_Query( array(
     'post_type' => $posttype,
     'orderby' => 'name',
     'order' => 'ASC',
 ) ); ?>
-<ul class="is-flex-container columns-3 wp-block-post-template wp-block-terms">
+<ul class="is-flex-container columns-<?php echo $cols ?> wp-block-post-template-container wp-block-post-template wp-block-other-habitats<?php echo esc_attr($className); ?>">
 <?php while ( $loop->have_posts() ) : $loop->the_post();
     $featured_img = get_the_post_thumbnail(get_the_ID(), 'swiper-thumb');
     $title = get_the_title();
