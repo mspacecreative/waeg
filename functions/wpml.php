8f84d36f55e58e8ba3d2da152abb0018448d5aa3
MattKCyr <matt@mspacecreative.com>
2023-07-19 09:08:52 -0300

    Update to WPML function from support ticket

--- functions/wpml.php
+++ functions/wpml.php
@@ -1,17 +1,18 @@
 <?php
+// WPML function to fix the translation of a template /fr/plante/especes/baies/
 function wpmlsupp_10867( $templates ) {
     if ( current_theme_supports( 'block-templates' ) ) {
         $current_term = get_queried_object();
- 
+  
         do_action( 'wpml_switch_language', apply_filters( 'wpml_default_language', null ) );
-        $original_term = get_term($current_term->term_id);
+        $orignal_term = get_term($current_term->term_id);
         do_action( 'wpml_switch_language', null );
- 
-        if (isset($original_term->slug) && $original_term->slug !== $current_term->slug ) {
-            array_push($templates, $original_term->taxonomy . '-' . $original_term->slug);    
+  
+        if (isset($orignal_term->slug) && $orignal_term->slug !== $current_term->slug ) {
+            array_unshift($templates, 'taxonomy-' . $orignal_term->taxonomy . '-' . $orignal_term->slug);    
         }
     }
- 
+  
     return $templates;
-}
-add_filter('taxonomy_template_hierarchy', 'wpmlsupp_10867');
+  }
+  add_filter('taxonomy_template_hierarchy', 'wpmlsupp_10867');
