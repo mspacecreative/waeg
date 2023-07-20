b48468c10bb8f73fc1425ba88864ff3622461b10
MattKCyr <matt@mspacecreative.com>
2023-07-20 15:32:45 -0300

    c

--- templates/blocks/block-traditional-names.php
+++ templates/blocks/block-traditional-names.php
@@ -32,7 +32,7 @@ if ( have_rows('traditional_names', get_the_ID()) ) {
             the_row();
             if (!empty(get_sub_field('siglitun_name') || get_sub_field('uummarmiutun') || get_sub_field('kangiryarmiutun')))
             echo
-            '<strong class="underlined-heading font-weight-normal font-size-22">' . esc_html_x('Inuvialuktun', 'waeg') . '</strong>';
+            '<strong class="underlined-heading font-size-22">' . esc_html_x('Inuvialuktun', 'waeg') . '</strong>';
         }
         echo
         '<ul class="no-bullets">';
