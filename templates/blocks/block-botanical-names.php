fb285090eae8757c5551ec5c05dfc98d5b38c1cb
MattKCyr <matt@mspacecreative.com>
2023-11-08 12:32:08 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -5,13 +5,14 @@ if (have_rows('botanical_names', get_the_ID())) {
     while (have_rows('botanical_names', get_the_ID())) {
         the_row();
 
+        $plural = 0;
+        $names = get_field('botanical_names');
+        if (is_array($names)) {
+            $plural = count($names) > 1 ? 's' : '';
+            echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $plural . ':</strong>';
+        }
+
         if (have_rows('botanical_name', get_the_ID())) {
-            $plural = 0;
-            $names = get_field('botanical_name');
-            if (is_array($names)) {
-                $plural = count($names) > 1 ? 's' : '';
-                echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $plural . ':</strong>';
-            }
             
             while (have_rows('botanical_name', get_the_ID())) {
                 the_row();
