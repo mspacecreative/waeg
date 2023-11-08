10247419d2031272d988c463b7bd36ec760b6d88
MattKCyr <matt@mspacecreative.com>
2023-11-08 12:33:57 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -2,15 +2,14 @@
 if (have_rows('botanical_names', get_the_ID())) {
     echo
     '<div class="botanical-names bottom-margin-40">';
-    while (have_rows('botanical_names', get_the_ID())) {
-        the_row();
-
         $plural = 0;
         $names = get_field('botanical_names');
         if (is_array($names)) {
             $plural = count($names) > 1 ? 's' : '';
         }
         echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $plural . ':</strong>';
+    while (have_rows('botanical_names', get_the_ID())) {
+        the_row();
 
         if (have_rows('botanical_name', get_the_ID())) {
             
