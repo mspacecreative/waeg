7ebaf8401343b32f1ef38dd61c0448ce2f8861c1
MattKCyr <matt@mspacecreative.com>
2023-11-08 15:43:37 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -20,7 +20,7 @@ if (have_rows('botanical_names', get_the_ID())) {
                 $name = get_sub_field('name');
                 $designation = get_sub_field('designation') ? get_sub_field('designation') : '';
                 $string = ' <span style="font-style: italic;">' . $name . '</span>' . esc_html(' ') . $designation . $separator;
-                print_r($string);
+                // print_r($string);
                 // echo substr($string, 0, -1);
             }
         }
