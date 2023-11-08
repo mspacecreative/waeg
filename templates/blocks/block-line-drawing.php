0a244838ab9a41f52a6fc7a4e65c9b054ea6989e
MattKCyr <matt@mspacecreative.com>
2023-11-08 11:53:10 -0400

    c

--- templates/blocks/block-line-drawing.php
+++ templates/blocks/block-line-drawing.php
@@ -7,5 +7,5 @@ if ( !empty($linedrawing) ): ?>
 <?php endif;
 
 if ( !empty($linedrawing2) ): ?>
-<img class="full-width object-fit-contain max-height-600 line-drawing" src="<?php echo esc_url($linedrawing2['url']); ?>" alt="<?php echo esc_attr($linedrawing2['alt']); ?>" />
+<img class="full-width object-fit-contain max-height-600 line-drawing top-margin-40" src="<?php echo esc_url($linedrawing2['url']); ?>" alt="<?php echo esc_attr($linedrawing2['alt']); ?>" />
 <?php endif;
