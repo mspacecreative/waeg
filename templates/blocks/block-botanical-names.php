31697cca04bda6956c381ce7f3e8a0b4832c730f
MattKCyr <matt@mspacecreative.com>
2023-11-08 12:44:42 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -2,9 +2,9 @@
 if (have_rows('botanical_names', get_the_ID())) {
     echo
     '<div class="botanical-names bottom-margin-40">';
+        $count = 0;    
         $names = get_field_object('botanical_names');
         $count = count($names);
-        $names = get_row('botanical_names');
         if ($count > 1) {
             echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical names', 'waeg') . ': </strong>';
         } else {
