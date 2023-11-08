6784c8a38ead9e61a6152f45164538e3c429370b
MattKCyr <matt@mspacecreative.com>
2023-11-08 11:49:27 -0400

    Added additional image upload field for line drawing

--- templates/blocks/block-line-drawing.php
+++ templates/blocks/block-line-drawing.php
@@ -1,5 +1,11 @@
 <?php
 $linedrawing = get_field('drawing', get_the_ID());
+$linedrawing2 = get_field('drawing_2', get_the_ID());
+
 if ( !empty($linedrawing) ): ?>
 <img class="full-width object-fit-contain max-height-600 line-drawing" src="<?php echo esc_url($linedrawing['url']); ?>" alt="<?php echo esc_attr($linedrawing['alt']); ?>" />
+<?php endif;
+
+if ( !empty($linedrawing2) ): ?>
+<img class="full-width object-fit-contain max-height-600 line-drawing" src="<?php echo esc_url($linedrawing2['url']); ?>" alt="<?php echo esc_attr($linedrawing2['alt']); ?>" />
 <?php endif;
