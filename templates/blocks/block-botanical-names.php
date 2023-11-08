7f4c9f059d599d9519528e2eba0805226658c389
MattKCyr <matt@mspacecreative.com>
2023-11-08 12:11:11 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -2,13 +2,20 @@
 if (have_rows('botanical_names', get_the_ID())) {
     while (have_rows('botanical_names', get_the_ID())) {
         the_row();
-        $botanical_names = get_sub_field('botanical_name');
-        $designation = get_sub_field('designation') ? get_sub_field('designation') : '';
-        if ( $botanical_names ) {
-            echo
-            '<div class="botanical-names bottom-margin-40">
-                <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name(s)', 'waeg') . ':</strong> ' . '<span style="font-style: italic;">' . $botanical_names . '</span>' . esc_html(' ') . $designation . '</h2>
-            </div>';
+
+        if (have_rows('botanical_name', get_the_ID())) {
+            while (have_rows('botanical_name', get_the_ID())) {
+                the_row();
+                $name = get_sub_field('name');
+                $designation = get_sub_field('designation') ? get_sub_field('designation') : '';
+                $plural = count('botanical_name') > 1 ? 's' : '';
+                if ( $name ) {
+                    echo
+                    '<div class="botanical-names bottom-margin-40">
+                        <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $plural . ':</strong> ' . '<span style="font-style: italic;">' . $name . '</span>' . esc_html(' ') . $designation . '</h2>
+                    </div>';
+                }
+            }
         }
     }
 }
