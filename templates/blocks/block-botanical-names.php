e79b9c322293a27a9b089329dbabe7b3c7e02629
MattKCyr <matt@mspacecreative.com>
2023-11-08 14:25:38 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -27,9 +27,9 @@ if (have_rows('botanical_names', get_the_ID())) {
                     $merge = array_combine($names_array, $designation_array);
                     $string = '';
                     foreach($merge as $key => $value) {
-                        $string .= "$key $value . '; ";
+                        $string .= "$key $value; ";
                     }
-                    $string = substr($string, 0, -1);
+                    $string = substr($string, 0, -2);
                     echo $string;
                 }
             }
