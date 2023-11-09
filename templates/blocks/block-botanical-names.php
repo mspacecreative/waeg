40b9676344f37a8fea05ff9f90a509c2b22ded34
MattKCyr <matt@mspacecreative.com>
2023-11-09 08:18:55 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -2,16 +2,14 @@
 if (have_rows('botanical_names', get_the_ID())) {
     echo
     '<div class="botanical-names bottom-margin-40">';
-    while (have_rows('botanical_names', get_the_ID())) {
-        the_row();
-
         $count = 0;    
         $names = get_field('botanical_names', get_the_ID());
         $count = count($names) > 1 ? 's' : '';
-        if (count($names) > 0) {
         echo 
         '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
-        }
+    while (have_rows('botanical_names', get_the_ID())) {
+        the_row();
+
         if (have_rows('botanical_name', get_the_ID())) {
             while (have_rows('botanical_name', get_the_ID())) {
                 the_row();
@@ -20,15 +18,13 @@ if (have_rows('botanical_names', get_the_ID())) {
                 $combine = array_combine($array, $designation);
             }
         }
-
+    }
         if ($combine) {
-            foreach($combine as $k => $v) {
-                $data[] = " <i>$k</i> $v";
-            }
+        foreach($combine as $k => $v) {
+            $data[] = " <i>$k</i> $v";
+        }
             echo implode('; ', $data); }
         echo 
-        '</h2>';
-    }
-    echo
-    '</div>';
+        '</h2>
+    </div>';
 }
