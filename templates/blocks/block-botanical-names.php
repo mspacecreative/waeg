3c961048ba236936a2c4ce511238301840784e21
MattKCyr <matt@mspacecreative.com>
2023-11-09 08:16:12 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -8,9 +8,10 @@ if (have_rows('botanical_names', get_the_ID())) {
         $count = 0;    
         $names = get_field('botanical_names', get_the_ID());
         $count = count($names) > 1 ? 's' : '';
+        if (count($names) > 0) {
         echo 
         '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
-
+        }
         if (have_rows('botanical_name', get_the_ID())) {
             while (have_rows('botanical_name', get_the_ID())) {
                 the_row();
