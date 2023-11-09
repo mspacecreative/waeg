662c2d61468aae16f850ab99e99b6dffaaaa3d78
MattKCyr <matt@mspacecreative.com>
2023-11-09 12:08:12 -0400

    c

--- templates/blocks/block-uses.php
+++ templates/blocks/block-uses.php
@@ -1,8 +1,14 @@
 <?php
+// CUSTOM CLASS	
+$className = '';
+if( !empty($block['className']) ) {
+	$className .= ' ' . $block['className'];
+}
+
 $uses = get_field('traditional_uses', get_the_ID());
 if ( $uses ) {
     echo 
-    '<div class="top-margin-40 has-medium-font-size">
+    '<div class="top-margin-40 has-medium-font-size' . esc_attr($className) . '">
         <h2 class="has-large-font-size">' . esc_html__('Traditional Uses', 'waeg') . '</h2>';
     echo
         '<div class="notes">' . $uses . '</div>
