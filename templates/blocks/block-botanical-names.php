30d555ba7aab98ab62a9756dbcae9a7f9b6ba5ef
MattKCyr <matt@mspacecreative.com>
2023-11-09 07:23:22 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -5,14 +5,11 @@ if (have_rows('botanical_names', get_the_ID())) {
         $count = 0;    
         $names = get_field('botanical_names', get_the_ID());
         $count = count($names) > 1 ? 's' : '';
-        if (count($names) !== 0) {
-            echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
-        }
+        echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
     while (have_rows('botanical_names', get_the_ID())) {
         the_row();
 
         if (have_rows('botanical_name', get_the_ID())) {
-            $separator = ';';
             while (have_rows('botanical_name', get_the_ID())) {
                 the_row();
                 $array[] = get_sub_field('name');
