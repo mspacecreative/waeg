3fbf434449c2702a449a0c720751b9d99dfc12d0
MattKCyr <matt@mspacecreative.com>
2023-11-09 08:14:46 -0400

    c

--- templates/blocks/block-botanical-names.php
+++ templates/blocks/block-botanical-names.php
@@ -2,12 +2,14 @@
 if (have_rows('botanical_names', get_the_ID())) {
     echo
     '<div class="botanical-names bottom-margin-40">';
+    while (have_rows('botanical_names', get_the_ID())) {
+        the_row();
+
         $count = 0;    
         $names = get_field('botanical_names', get_the_ID());
         $count = count($names) > 1 ? 's' : '';
-        echo '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
-    while (have_rows('botanical_names', get_the_ID())) {
-        the_row();
+        echo 
+        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $count . ':</strong>';
 
         if (have_rows('botanical_name', get_the_ID())) {
             while (have_rows('botanical_name', get_the_ID())) {
@@ -17,12 +19,15 @@ if (have_rows('botanical_names', get_the_ID())) {
                 $combine = array_combine($array, $designation);
             }
         }
+
+        if ($combine) {
+            foreach($combine as $k => $v) {
+                $data[] = " <i>$k</i> $v";
+            }
+            echo implode('; ', $data); }
+        echo 
+        '</h2>';
     }
-    if ($combine) {
-    foreach($combine as $k => $v) {
-        $data[] = " <i>$k</i> $v";
-    }
-                echo implode('; ', $data); }
-            echo '</h2>
-    </div>';
+    echo
+    '</div>';
 }
