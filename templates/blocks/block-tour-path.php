bb06fc4f03575d8c3f2ecfd2043a6d6e4899ce96
MattKCyr <matt@mspacecreative.com>
2023-11-09 16:32:59 -0400

    c

--- templates/blocks/block-tour-path.php
+++ templates/blocks/block-tour-path.php
@@ -15,7 +15,7 @@ $loop = new WP_Query( array(
 <?php while ( $loop->have_posts() ) : $loop->the_post();
     
     // VARIABLES
-    $line_drawing = get_field('drawing', $loop->ID);
+    $line_drawing = get_field('drawing', get_the_ID());
     $title = get_the_title($loop->ID);
     $excerpt = get_the_excerpt($loop->ID) ? '<p class="text__small">' . get_the_excerpt($loop->ID) . '</p>' : '';
     $permalink = get_the_permalink($loop->ID);
