5f83a8c7f0bac55db6fe784948da394342894f6c
MattKCyr <matt@mspacecreative.com>
2023-11-10 12:38:13 -0400

    c

--- templates/blocks/includes/template-notes.php
+++ templates/blocks/includes/template-notes.php
@@ -3,7 +3,7 @@ $notes = get_field('notes', get_the_ID($term_id));
 if ( $notes ) {
     echo
     '<div class="notes">
-        <h2 class="has-large-font-size">' . esc_html_x('Notes', 'waeg') . '</h2>'
+        <h2 class="has-large-font-size" style="margin-block-start: 0;">' . esc_html_x('Notes', 'waeg') . '</h2>'
          . $notes . 
     '</div>';
 }
--- templates/blocks/includes/template-traditional-uses.php
+++ templates/blocks/includes/template-traditional-uses.php
@@ -2,8 +2,8 @@
 $uses = get_field('traditional_uses', get_the_ID($term_id));
 if ( $uses ) {
     echo 
-    '<div class="has-medium-font-size">
-        <h2 class="has-large-font-size">' . esc_html__('Traditional Uses', 'waeg') . '</h2>';
+    '<div>
+        <h2 class="has-large-font-size" style="margin-block-start: 0;">' . esc_html__('Traditional Uses', 'waeg') . '</h2>';
     echo
         '<div class="notes">' . $uses . '</div>
     </div>';
