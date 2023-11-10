9922834b5a8e5f0925e609e237acaac6f9d28e35
MattKCyr <matt@mspacecreative.com>
2023-11-10 14:14:41 -0400

    c

--- templates/blocks/block-tour-path.php
+++ templates/blocks/block-tour-path.php
@@ -42,8 +42,11 @@ $loop = new WP_Query( array(
         echo 
         '<div class="tour-path-content">
             <h2 class="wp-block-post-title has-large-font-size">' . esc_html__($title) . '</h2>'
-            . $excerpt, $tour_url .
-            '<a class="button post-modal-link" data-id="bio-' . $count++ . '" href="' . esc_url($permalink) . '">' . esc_html_x('Berry Details', 'waeg') . '</a>
+            . $excerpt .
+            '<div class="is-layout-flex">'
+                . $tour_url .
+                '<a class="button post-modal-link" data-id="bio-' . $count++ . '" href="' . esc_url($permalink) . '">' . esc_html_x('Berry Details', 'waeg') . '</a>
+            </div>
         </div>';
 
         echo
