4c278a66d375f75fd90a8f801851c0f364f37764
MattKCyr <matt@mspacecreative.com>
2023-11-10 11:36:10 -0400

    c

--- templates/blocks/block-post-modal.php
+++ templates/blocks/block-post-modal.php
@@ -34,8 +34,10 @@ if ($loop->have_posts()) {
                         <span style="background-color: #000;">&nbsp;</span>
                         <span style="background-color: #000;">&nbsp;</span>
                     </button>
-                    <img src="' . $line_drawing['url'] . '" alt="' . $line_drawing['alt'] . '">
-                </div>';
+                    <img src="' . $line_drawing['url'] . '" alt="' . $line_drawing['alt'] . '">';
+                    include 'includes/template-carousel.php';
+                echo
+                '</div>';
             }
             echo
             '</div>
