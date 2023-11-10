520d9300b8455f7fb05ce2082f67f617e1eda350
MattKCyr <matt@mspacecreative.com>
2023-11-10 12:35:57 -0400

    c

--- templates/blocks/includes/template-traditional-uses.php
+++ templates/blocks/includes/template-traditional-uses.php
@@ -2,7 +2,7 @@
 $uses = get_field('traditional_uses', get_the_ID($term_id));
 if ( $uses ) {
     echo 
-    '<div class="top-margin-40 has-medium-font-size">
+    '<div class="has-medium-font-size">
         <h2 class="has-large-font-size">' . esc_html__('Traditional Uses', 'waeg') . '</h2>';
     echo
         '<div class="notes">' . $uses . '</div>
