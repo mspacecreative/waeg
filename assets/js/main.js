d25413db9e265317b10444deb89dc4ce37ec649a
MattKCyr <matt@mspacecreative.com>
2023-11-10 12:09:44 -0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ for (i = 0; i < closeModalButton.length; i++) {
 modal.addEventListener('click', function() {
   this.classList.remove('show');
   modalBackdrop.classList.toggle('show');
-  this.querySelectorAll('.post-modal-content').classList.remove('show');
+  this.postModalContent.classList.remove('show');
 });
 
 for (i = 0; i < postModalContent.length; i++) {
