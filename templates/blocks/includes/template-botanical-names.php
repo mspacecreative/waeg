c3e42d4afe1de443fdf03fb3e06a2d06b2dfc75c
MattKCyr <matt@mspacecreative.com>
2023-11-11 12:52:12 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -2,18 +2,18 @@
 if (have_rows('botanical_names', get_the_ID($term_id))) {
     echo
     '<div class="botanical-names">';
-        $names = get_field('botanical_names', get_the_ID($term_id));
+        $names = get_field('botanical_names');
         $check_plural = count($names) > 1 ? 's' : '';
         echo 
         '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
 
-        if (have_rows('botanical_name', get_the_ID($term_id))) {
-            while (have_rows('botanical_name', get_the_ID($term_id))) {
+        if (have_rows('botanical_name') {
+            while (have_rows('botanical_name')) {
                 the_row();
-                $array[] = get_sub_field('name', get_the_ID($term_id));
-                $designation[] = get_sub_field('designation', get_the_ID($term_id));
+                $array[] = get_sub_field('name');
+                $designation[] = get_sub_field('designation');
                 $combine = array_combine($array, $designation);
             }
         }
