bc063375bf38d8b0cc42a7dad75c804d9e295412
MattKCyr <matt@mspacecreative.com>
2023-11-11 13:59:57 -0400

    c

--- templates/blocks/includes/template-traditional-uses.php
+++ templates/blocks/includes/template-traditional-uses.php
@@ -3,8 +3,9 @@ $uses = get_field('traditional_uses', get_the_ID($term_id));
 if ( $uses ) {
     echo 
     '<div>
-        <h2 class="has-large-font-size" style="margin-block-start: 0;">' . esc_html__('Traditional Uses', 'waeg') . '</h2>';
-    echo
-        '<div class="notes">' . $uses . '</div>
+        <h2 class="has-large-font-size" style="margin-block-start: 0;">' . esc_html__('Traditional Uses', 'waeg') . '</h2>
+        <div class="notes">' 
+            . $uses . 
+        '</div>
     </div>';
 }
