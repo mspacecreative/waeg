e5a41fc349368f81419dfbf1d3c1818a126d31ec
MattKCyr <matt@mspacecreative.com>
2023-11-11 12:46:09 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -2,7 +2,6 @@
 if (have_rows('botanical_names', get_the_ID($term_id))) {
     echo
     '<div class="botanical-names">';
-        $count = 0;    
         $names = get_field('botanical_names', get_the_ID($term_id));
         $check_plural = count($names) > 1 ? 's' : '';
         echo 
