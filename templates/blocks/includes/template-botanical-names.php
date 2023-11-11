6a73d968cebc7fe35c497eeaa4fb401525e994ed
MattKCyr <matt@mspacecreative.com>
2023-11-11 12:53:50 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,12 +1,12 @@
 <?php
-if (have_rows('botanical_names', get_the_ID($term_id))) {
+if (have_rows('botanical_names')) {
     echo
     '<div class="botanical-names">';
         $names = get_field('botanical_names');
         $check_plural = count($names) > 1 ? 's' : '';
         echo 
         '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-    while (have_rows('botanical_names', get_the_ID($term_id))) {
+    while (have_rows('botanical_names')) {
         the_row();
 
         if (have_rows('botanical_name')) {
