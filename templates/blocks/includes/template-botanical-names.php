fd9768c122879ee0870e23e5058fd3ad05cf1fa9
MattKCyr <matt@mspacecreative.com>
2023-11-11 13:12:00 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,16 +1,16 @@
 <?php
-if (have_rows('botanical_names', get_the_ID($term_id))) {
+if (have_rows('botanical_names', get_the_ID())) {
     echo
     '<div class="botanical-names">';
-        $names = get_field('botanical_names', get_the_ID($term_id));
+        $names = get_field('botanical_names', get_the_ID());
         $check_plural = count($names) > 1 ? 's' : '';
         echo 
         '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-    while (have_rows('botanical_names', get_the_ID($term_id))) {
+    while (have_rows('botanical_names', get_the_ID())) {
         the_row();
 
-        if (have_rows('botanical_name', get_the_ID($term_id))) {
-            while (have_rows('botanical_name', get_the_ID($term_id))) {
+        if (have_rows('botanical_name', get_the_ID())) {
+            while (have_rows('botanical_name', get_the_ID())) {
                 the_row();
                 $array[] = get_sub_field('name');
                 $designation[] = get_sub_field('designation');
@@ -18,21 +18,13 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
             }
         }
     }
-        // if ($combine) {
-        // foreach($combine as $k => $v) {
-        //     $v_check = $v ? ' ' . $v : '';
-        //     $data[] = " <i>$k</i>$v_check";
-        // }
-        //     echo implode('; ', $data); }
+        if ($combine) {
+        foreach($combine as $k => $v) {
+            $v_check = $v ? ' ' . $v : '';
+            $data[] = " <i>$k</i>$v_check";
+        }
+            echo implode('; ', $data); }
         echo 
-        // '</h2>
-    '</div>';
+        '</h2>
+    </div>';
 }
-if ($combine) {
-    foreach($combine as $k => $v) {
-        $v_check = $v ? ' ' . $v : '';
-        $data[] = " <i>$k</i>$v_check";
-    }
-        echo implode('; ', $data); }
-    echo 
-    '</h2>';
