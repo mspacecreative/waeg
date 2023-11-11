d047d9191c58d62d4a6a9529dda04673a963f548
MattKCyr <matt@mspacecreative.com>
2023-11-11 13:36:09 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,11 +1,7 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
-    echo
-    '<div class="botanical-names">';
-        $names = get_field('botanical_names', get_the_ID($term_id));
-        $check_plural = count($names) > 1 ? 's' : '';
-        echo 
-        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+    $names = get_field('botanical_names', get_the_ID($term_id));
+    $check_plural = count($names) > 1 ? 's' : '';
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
 
@@ -28,3 +24,17 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
         '</h2>
     </div>';
 }
+
+if ($combine) {
+    foreach($combine as $k => $v) {
+        $v_check = $v ? ' ' . $v : '';
+        $data[] = " <i>$k</i>$v_check";
+    }
+}
+echo
+'<div class="botanical-names">
+    <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+        echo implode('; ', $data); 
+    echo 
+    '</h2>
+</div>';
