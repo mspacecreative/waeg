d260ea4dd6d9b243fa55b92e4b9f524c0e62d40b
MattKCyr <matt@mspacecreative.com>
2023-11-14 13:25:10 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -34,21 +34,24 @@
 //         '</h2>
 //     </div>';
 // }
-
-if (have_rows('botanical_name', get_the_ID($term_id))) {
-    while (have_rows('botanical_name', get_the_ID($term_id))) {
-        the_row();
-        $row = get_row();
-        $names_array[] = $row['name'];
-        $designation_array[] = $row['designation'];
-        $combined_arrays = array_combine($names_array, $designation_array);
-    }
-    if ($combined_arrays) {
-        foreach($combined_arrays as $k => $v) {
-            $v_check = $v ? ' ' . $v : '';
+if (have_rows('botanical_names', get_the_ID($term_id))) {
+    while (have_rows('botanical_names', get_the_ID($term_id))) {
+        if (have_rows('botanical_name')) {
+            while (have_rows('botanical_name')) {
+                the_row();
+                $row = get_row();
+                $names_array[] = $row['name'];
+                $designation_array[] = $row['designation'];
+                $combined_arrays = array_combine($names_array, $designation_array);
+            }
+            if ($combined_arrays) {
+                foreach($combined_arrays as $k => $v) {
+                    $v_check = $v ? ' ' . $v : '';
+                }
+            }
+            
+            $data[] = " <i>$k</i>$v_check";
+            echo implode('; ', $data);
         }
     }
-    
-    $data[] = " <i>$k</i>$v_check";
-    echo implode('; ', $data);
 }
