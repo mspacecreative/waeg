1513633ef08b1d6e48de099e18888b4fba4a825e
MattKCyr <matt@mspacecreative.com>
2023-11-14 16:10:15 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -26,15 +26,7 @@
 
 $names = get_field('botanical_name', get_the_ID($term_id));
 if ($names) {
-    foreach($names as $k => $v) {
-        $v_check = $v ? ' ' . $v : '';
-        $data = " <i>$k</i>$v_check";
+    foreach($names as $name) {
+        echo $name;
     }
 }
-
-echo
-'<div class="botanical-names">
-    <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . ':</strong>';
-        echo $data . 
-    '</h2>
-</div>';
