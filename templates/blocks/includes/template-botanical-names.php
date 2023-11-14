0775333803c8cbeb2e193612866053afd7dd6024
MattKCyr <matt@mspacecreative.com>
2023-11-14 15:39:15 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,38 +1,30 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
     
-    // echo
-    // '<div class="botanical-names">';
-
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
 
         $names = get_row_index(get_the_ID($term_id));
         $check_plural = $names > 1 ? 's' : '';
             
-        // echo 
-        // '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-
         if (have_rows('botanical_name')) {
             while (have_rows('botanical_name')) {
                 the_row();
                 $names_array[] = get_sub_field('name');
                 $designation_array[] = get_sub_field('designation');
+                $combine = array_combine($names_array, $designation_array);
             }
         }
-        // echo 
-        // '</h2>';
     }
-    $combine = array_combine($names_array, $designation_array);
+}
 
-    // echo
-    // '</div>';
-}
 print_r($combine);
-foreach($combine as $k => $v) {
+
+foreach($combine as $k => $v)
     $v_check = $v ? ' ' . $v : '';
-}
-$data[] = " <i>$k</i>$v_check";
+
+    $data[] = " <i>$k</i>$v_check";
+
 echo
 '<div class="botanical-names">
     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
