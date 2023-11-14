092ac41cbe6908e6bb8baf754da3d7c9b64efc2a
MattKCyr <matt@mspacecreative.com>
2023-11-14 16:13:46 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -15,15 +15,15 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                 $combine = array_combine($names_array, $designations_array);
             }
         }
-        foreach($combine as $k => $v) {
-            $v_check = $v ? ' ' . $v : '';
-            $data[] = " <i>$k</i>$v_check";
-        }
     }
-    echo
-    '<div class="botanical-names">
-        <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-            echo implode('; ', $data) . 
-        '</h2>
-    </div>';
 }
+foreach($combine as $k => $v) {
+    $v_check = $v ? ' ' . $v : '';
+    $data[] = " <i>$k</i>$v_check";
+}
+echo
+'<div class="botanical-names">
+    <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+        echo implode('; ', $data) . 
+    '</h2>
+</div>';
