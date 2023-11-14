3db527214191eaa0c61e33f6ebdc45b5676a4ed0
MattKCyr <matt@mspacecreative.com>
2023-11-14 15:02:17 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,16 +1,17 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
+    
     echo
     '<div class="botanical-names">';
 
-    $names = get_row_index();
-    $check_plural = $names > 1 ? 's' : '';
-        
-        echo 
-        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
+
+        $names = get_row_index();
+        $check_plural = $names > 1 ? 's' : '';
+            
+        echo 
+        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
         if (have_rows('botanical_name')) {
             while (have_rows('botanical_name')) {
@@ -28,10 +29,10 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                 echo implode('; ', $data);
             }
         }
-    }
         
         echo 
         '</h2>';
+    }
 
     echo
     '</div>';
