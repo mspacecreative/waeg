04cc45c0b94abd8a6ef41b64a010087e5ad7e7bd
MattKCyr <matt@mspacecreative.com>
2023-11-14 16:29:35 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -6,26 +6,34 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
 
         $names = get_row_index();
         $check_plural = $names > 1 ? 's' : '';
-            
-        if (have_rows('botanical_name', $loop->ID)) {
-            while (have_rows('botanical_name', $loop->ID)) {
-                the_row();
 
-                if ( $plant_names = get_row() ) {
-                    foreach ($plant_names as $key => $value) {
-                        $value = get_sub_field('designation');
-                        if (!empty($value) ) { 
-                            $field = get_sub_field('name', $key);
-                            echo
-                            '<li class="has-medium-font-size">' . '<strong class="color--dark-green">' . $field . ':</strong> ' . $value . '</li>';
+        echo
+        '<div class="botanical-names">
+            <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+            
+            if (have_rows('botanical_name', $loop->ID)) {
+                while (have_rows('botanical_name', $loop->ID)) {
+                    the_row();
+
+                    if ( $plant_names = get_row() ) {
+                        foreach ($plant_names as $key => $value) {
+                            $value = get_sub_field('designation');
+                            if (!empty($value) ) { 
+                                $field = get_sub_field('name', $key);
+                                echo
+                                ' <i>' . $field . '</i> ' . $value . '; ';
+                            }
                         }
                     }
+                    // $names_array[] = get_sub_field('name', $loop->ID);
+                    // $designations_array[] = get_sub_field('designation', $loop->ID);
+                    // $combine = array_combine($names_array, $designations_array);
                 }
-                // $names_array[] = get_sub_field('name', $loop->ID);
-                // $designations_array[] = get_sub_field('designation', $loop->ID);
-                // $combine = array_combine($names_array, $designations_array);
             }
-        }
+            
+            echo
+            '</h2>
+        </div>'
     }
 }
 // foreach($combine as $k => $v) {
