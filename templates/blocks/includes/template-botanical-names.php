41e0da11ae0a267d0a4bf22b84d10abe15eb326a
MattKCyr <matt@mspacecreative.com>
2023-11-14 13:30:11 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,58 +1,30 @@
 <?php
-// if (have_rows('botanical_names', get_the_ID($term_id))) {
-//     $names = get_field('botanical_names', get_the_ID($term_id));
-//     $check_plural = count($names) > 1 ? 's' : '';
-
-//     echo
-//     '<div class="botanical-names">
-//         <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-//     while (have_rows('botanical_names', get_the_ID($term_id))) {
-//         the_row();
-
-
-
-//             if (have_rows('botanical_name')) {
-//                 while (have_rows('botanical_name')) {
-//                     the_row();
-//                     $row = get_row();
-//                     $names_array[] = $row['name'];
-//                     $designation_array[] = $row['designation'];
-//                     $combined_arrays = array_combine($names_array, $designation_array);
-//                 }
-//             }
-//     }
-//     if ($combined_arrays) {
-//         foreach($combined_arrays as $k => $v) {
-//             $v_check = $v ? ' ' . $v : '';
-//         }
-//     }
-    
-//     $data[] = " <i>$k</i>$v_check";
-//     echo implode('; ', $data);
-
-//         echo
-//         '</h2>
-//     </div>';
-// }
 if (have_rows('botanical_names', get_the_ID($term_id))) {
+    echo
+    '<div class="botanical-names">';
+    $names = get_field('botanical_names', get_the_ID($term_id));
+    $check_plural = count($names) > 1 ? 's' : '';
+        echo 
+        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
-        if (have_rows('botanical_name', get_the_ID($term_id))) {
-            while (have_rows('botanical_name', get_the_ID($term_id))) {
+
+        if (have_rows('botanical_name')) {
+            while (have_rows('botanical_name')) {
                 the_row();
-                $row = get_row();
-                $names_array[] = $row['name'];
-                $designation_array[] = $row['designation'];
-                $combined_arrays = array_combine($names_array, $designation_array);
+                $array[] = get_sub_field('name');
+                $designation[] = get_sub_field('designation');
+                $combine = array_combine($array, $designation);
             }
-            if ($combined_arrays) {
-                foreach($combined_arrays as $k => $v) {
-                    $v_check = $v ? ' ' . $v : '';
-                }
-            }
-            
-            $data[] = " <i>$k</i>$v_check";
-            echo implode('; ', $data);
         }
     }
+        if ($combine) {
+        foreach($combine as $k => $v) {
+            $v_check = $v ? ' ' . $v : '';
+        }
+        $data[] = " <i>$k</i>$v_check";
+            echo implode('; ', $data); }
+        echo 
+        '</h2>
+    </div>';
 }
