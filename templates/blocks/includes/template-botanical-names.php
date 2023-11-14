8a06e49fd504884f528eb9c3b01730ce8985d607
MattKCyr <matt@mspacecreative.com>
2023-11-14 13:45:27 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -20,9 +20,9 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
         if ($combine) {
             foreach($combine as $k => $v) {
                 $v_check = $v ? ' ' . $v : '';
-                $data[] = " <i>$k</i>$v_check";
             }
         }
+        $data[] = " <i>$k</i>$v_check";
             echo implode('; ', $data) . 
         '</h2>';
     }
