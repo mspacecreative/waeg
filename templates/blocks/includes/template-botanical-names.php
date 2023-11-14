289b13c550256b7d9a13dc8efa52c6873f31c7de
MattKCyr <matt@mspacecreative.com>
2023-11-14 16:26:14 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -10,20 +10,31 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
         if (have_rows('botanical_name', $loop->ID)) {
             while (have_rows('botanical_name', $loop->ID)) {
                 the_row();
-                $names_array[] = get_sub_field('name', $loop->ID);
-                $designations_array[] = get_sub_field('designation', $loop->ID);
-                $combine = array_combine($names_array, $designations_array);
+
+                if ( $plant_names = get_row() ) {
+                    foreach ($plant_names as $key => $value) {
+                        $value = get_sub_field('designation');
+                        if (!empty($value) ) { 
+                            $field = get_sub_field('name', $key);
+                            echo
+                            '<li class="has-medium-font-size">' . '<strong class="color--dark-green">' . $field . ':</strong> ' . $value . '</li>';
+                        }
+                    }
+                }
+                // $names_array[] = get_sub_field('name', $loop->ID);
+                // $designations_array[] = get_sub_field('designation', $loop->ID);
+                // $combine = array_combine($names_array, $designations_array);
             }
         }
     }
 }
-foreach($combine as $k => $v) {
-    $v_check = $v ? ' ' . $v : '';
-    $data[] = " <i>$k</i>$v_check";
-}
-echo
-'<div class="botanical-names">
-    <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-        echo implode('; ', $data) . 
-    '</h2>
-</div>';
+// foreach($combine as $k => $v) {
+//     $v_check = $v ? ' ' . $v : '';
+//     $data[] = " <i>$k</i>$v_check";
+// }
+// echo
+// '<div class="botanical-names">
+//     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+//         echo implode('; ', $data) . 
+//     '</h2>
+// </div>';
