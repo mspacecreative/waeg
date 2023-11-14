c4f5dce13f13461a833c5b2c3158765f5fb69fa5
MattKCyr <matt@mspacecreative.com>
2023-11-14 12:52:20 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,35 +1,34 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
-    echo
-    '<div class="botanical-names">';
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
 
         $names = get_field('botanical_names', get_the_ID($term_id));
         $check_plural = count($names) > 1 ? 's' : '';
-        
-        echo 
-        '<h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
-        if (have_rows('botanical_name')) {
-            while (have_rows('botanical_name')) {
-                the_row();
-                $names_array[] = get_sub_field('name');
-                $designation_array[] = get_sub_field('designation');
-                $combined_arrays = array_combine($names_array, $designation_array);
-
-                if ($combined_arrays) {
-                    foreach($combined_arrays as $k => $v) {
-                        $v_check = $v ? ' ' . $v : '';
-                    }
-                    $data[] = " <i>$k</i>$v_check";
-                }
-            }
-            echo implode('; ', $data);
-        }
         echo
-        '</h2>';
+        '<div class="botanical-names">
+            <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
+
+            if (have_rows('botanical_name')) {
+                while (have_rows('botanical_name')) {
+                    the_row();
+                    $names_array[] = get_sub_field('name');
+                    $designation_array[] = get_sub_field('designation');
+                    $combined_arrays = array_combine($names_array, $designation_array);
+
+                    if ($combined_arrays) {
+                        foreach($combined_arrays as $k => $v) {
+                            $v_check = $v ? ' ' . $v : '';
+                        }
+                        $data[] = " <i>$k</i>$v_check";
+                    }
+                }
+                echo implode('; ', $data);
+            }
+
+            echo
+            '</h2>
+        </div>';
     }
-    echo
-    '</div>';
 }
