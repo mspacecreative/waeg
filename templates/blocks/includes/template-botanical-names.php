aeeeec7169ed8965b3a2f713e6d29594081167ea
MattKCyr <matt@mspacecreative.com>
2023-11-14 13:15:57 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,14 +1,13 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
+    $names = get_field('botanical_names', get_the_ID($term_id));
+    $check_plural = count($names) > 1 ? 's' : '';
+
+    echo
+    '<div class="botanical-names">
+        <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
-
-        $names = get_field('botanical_names', get_the_ID($term_id));
-        $check_plural = count($names) > 1 ? 's' : '';
-
-        echo
-        '<div class="botanical-names">
-            <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
             if (have_rows('botanical_name')) {
                 while (have_rows('botanical_name')) {
@@ -18,18 +17,17 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                     $combined_arrays = array_combine($names_array, $designation_array);
                 }
             }
-            
-            if ($combined_arrays) {
-                foreach($combined_arrays as $k => $v) {
-                    $v_check = $v ? ' ' . $v : '';
-                }
-            }
-            
-            $data[] = " <i>$k</i>$v_check";
-            echo implode('; ', $data);
-
-            echo
-            '</h2>
-        </div>';
     }
+    if ($combined_arrays) {
+        foreach($combined_arrays as $k => $v) {
+            $v_check = $v ? ' ' . $v : '';
+        }
+    }
+    
+    $data[] = " <i>$k</i>$v_check";
+    echo implode('; ', $data);
+
+        echo
+        '</h2>
+    </div>';
 }
