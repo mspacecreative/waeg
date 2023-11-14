a179ffc82f6f900404f860fd761c974fb12e8316
MattKCyr <matt@mspacecreative.com>
2023-11-14 15:23:28 -0400

    c

--- templates/blocks/block-post-modal.php
+++ templates/blocks/block-post-modal.php
@@ -23,20 +23,20 @@ if ($loop->have_posts()) {
     '<div class="modal-backdrop"></div>
     <div class="modal">
         <div class="modal_table">
-            <div class="modal_table_cell">';
-            while ($loop->have_posts()) {
-                $loop->the_post();
-                $term_id = get_queried_object_id();
-                $title = get_the_title($loop->ID);
-                $line_drawing = get_field('drawing', get_the_ID($term_id));
-                echo
-                '<div id="bio-' . $count++ . '" class="post-modal-content">
+            <div class="modal_table_cell">
+                <div id="bio-' . $count++ . '" class="post-modal-content">
                     <button class="closeModalButton">
                         <span style="background-color: #000;">&nbsp;</span>
                         <span style="background-color: #000;">&nbsp;</span>
                     </button>
-                    <div class="post-modal-content-wrapper">
-                        <div class="wp-block-columns is-layout-flex are-vertically-aligned-center page-title">
+                    <div class="post-modal-content-wrapper">';
+                    while ($loop->have_posts()) {
+                        $loop->the_post();
+                        $term_id = get_queried_object_id();
+                        $title = get_the_title($loop->ID);
+                        $line_drawing = get_field('drawing', get_the_ID($term_id));
+                        echo
+                        '<div class="wp-block-columns is-layout-flex are-vertically-aligned-center page-title">
                             <div class="wp-block-column">
                                 <h1 class="has-large-font-size" style="margin: 0;">' . $title . '</h1>
                             </div>
@@ -56,12 +56,12 @@ if ($loop->have_posts()) {
                                 include 'includes/template-traditional-uses.php';
                             echo
                             '</div>
-                        </div>
-                    </div>
-                </div>';
-            }
-            echo
-            '</div>
+                        </div>';
+                    }
+                    echo
+                    '</div>
+                </div>
+            </div>
         </div>
     </div>';
-} rewind_posts();
+} wp_reset_query();
