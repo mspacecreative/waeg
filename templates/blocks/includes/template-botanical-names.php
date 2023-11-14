9eedea56b657cf2b36b7d68f92ec6b40193f2de8
MattKCyr <matt@mspacecreative.com>
2023-11-14 15:57:43 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -6,21 +6,24 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
 
         $names = get_row_index();
         $check_plural = $names > 1 ? 's' : '';
-    }
-}
-if (have_rows('botanical_name', get_the_ID($term_id))) {
-    while (have_rows('botanical_name', get_the_ID($term_id))) {
-        the_row();
-        $names_array[] = get_sub_field('name');
-        $designations_array[] = get_sub_field('designation');
-        $combine = array_combine($names_array, $designations_array);
+            
+        if (have_rows('botanical_name')) {
+            while (have_rows('botanical_name')) {
+                the_row();
+                $names_array[] = get_sub_field('name');
+                $designations_array[] = get_sub_field('designation');
+                $combine = array_combine($names_array, $designations_array);
+            }
+        }
     }
 }
 
-foreach($combine as $k => $v) {
-    $v_check = $v ? ' ' . $v : '';
+if ($combine) {
+    foreach($combine as $k => $v) {
+        $v_check = $v ? ' ' . $v : '';
+        $data[] = " <i>$k</i>$v_check";
+    }
 }
-$data[] = " <i>$k</i>$v_check";
 
 echo
 '<div class="botanical-names">
