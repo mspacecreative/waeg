b606ecf7b46dff82df82928739d2a83e56750543
MattKCyr <matt@mspacecreative.com>
2023-11-14 20:33:33 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -25,7 +25,7 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                             // $designation = $designation ? ' ' . get_sub_field('designation') . '; ' : '';
                             $name_array = explode(" ", $name);
                             $designation_array = explode(" ", $designation);
-                            $array = '<i>' . $name_array . '</i>' . $designation_array;
+                            $array = '<i>' . implode(' ', $name_array) . '</i>' . implode(" ", $designation_array);
                         }
                         print_r($array);
                         // echo $name, $designation;
