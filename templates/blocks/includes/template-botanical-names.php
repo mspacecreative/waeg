48d29e65bf5729f8f9151ad2b5ae1ce7a10dcaba
MattKCyr <matt@mspacecreative.com>
2023-11-15 11:06:34 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -22,8 +22,7 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                             $name = get_sub_field('name', $plant_name) ? ' <i>' . get_sub_field('name', $plant_name) . '</i>' : '';
                             $designation = get_sub_field('designation', $plant_name) ? ' ' . get_sub_field('designation', $plant_name) : '';
                         }
-                        $result = $name;
-                        $result .= $designation;
+                        $result = implode(' ', array($name, $designation));
                         $result = $name_count > 1 ? substr_replace($result, "", -2) : $result;
                         echo $result;
                     }
