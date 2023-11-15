24aecf5810de115073a140f1833c70bff19bb471
MattKCyr <matt@mspacecreative.com>
2023-11-15 15:17:44 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -15,8 +15,8 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                 while (have_rows('botanical_name')) {
                     the_row();
                     $row_count = get_row_index();
-                    $name_array[] = get_sub_field('name', get_the_ID($term_id));
-                    $designation_array[] = get_sub_field('designation', get_the_ID($term_id));
+                    $name_array[] = get_sub_field('name');
+                    $designation_array[] = get_sub_field('designation');
                     $combine = array_combine($name_array, $designation_array);
 
                     if ($combine) {
@@ -26,20 +26,12 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                     }
                 }
             }
-            $names_string[] = ' <i>' . $k . '</i>' . $v_check;
+            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
     }
-    echo implode('; ', $names_string);
+    echo implode('; ', $names_array);
 
     echo
         '</h2>
     </div>';
 }
-
-// echo
-// '<div class="botanical-names">
-//     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-//         echo implode('; ', $names_string);
-//     echo
-//     '</h2>
-// </div>';
 
