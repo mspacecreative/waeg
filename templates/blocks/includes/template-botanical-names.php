0c2f750786cad12658d6e83a878e21557bf45401
MattKCyr <matt@mspacecreative.com>
2023-11-15 16:09:09 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,34 +1,30 @@
 <?php
-// if (have_rows('botanical_names', get_the_ID($term_id))) {
+$name_count = count(get_field('botanical_names', get_the_ID($term_id)));
+$check_plural = $name_count > 1 ? 's' : '';
 
-    $name_count = count(get_field('botanical_names', get_the_ID($term_id)));
-    $check_plural = $name_count > 1 ? 's' : '';
+echo
+'<div class="botanical-names">
+    <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
-    echo
-    '<div class="botanical-names">
-        <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-    
-    while (have_rows('botanical_names', get_the_ID($term_id))) {
-        the_row();
+while (have_rows('botanical_names', get_the_ID($term_id))) {
+    the_row();
 
-            if (have_rows('botanical_name')) {
-                while (have_rows('botanical_name')) {
-                    the_row();
-                    $name_array[] = get_sub_field('name');
-                    $designation_array[] = get_sub_field('designation');
-                    $combined_array = array_combine($name_array, $designation_array);
+        while (have_rows('botanical_name')) {
+            the_row();
+            $name_array[] = get_sub_field('name');
+            $designation_array[] = get_sub_field('designation');
+            $combined_array = array_combine($name_array, $designation_array);
 
-                    foreach($combined_array as $k => $v) {
-                        $v_check = $v ? ' ' . $v : '';
-                    }
-                }
+            foreach($combined_array as $k => $v) {
+                $v_check = $v ? ' ' . $v : '';
             }
-            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
-    }
-    echo implode('; ', $names_array);
+        }
+        
+        $names_array[] = ' <i>' . $k . '</i>' . $v_check;
+}
+echo implode('; ', $names_array);
 
-    echo
-        '</h2>
-    </div>';
-// }
+echo
+    '</h2>
+</div>';
 
