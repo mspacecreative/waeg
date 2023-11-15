8c7dc771d4e1c4762c6108bd9fabfc3f24d71dba
MattKCyr <matt@mspacecreative.com>
2023-11-15 09:50:49 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -14,31 +14,29 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
             if (have_rows('botanical_name')) {
                 while (have_rows('botanical_name')) {
                     the_row();
+                    
+                    $row_index = get_row_index();
 
                     if ( $plant_names = get_row() ) {
-                        $count = count($plant_names);
-                        $name_array[] = get_sub_field('name');
-                        $designation_array[] = get_sub_field('designation');
-                        foreach ($plant_names as $k => $v) {
-                            $k = get_sub_field('name');
-                            $v = get_sub_field('designation');
-                            $result = ' <i>' . $k . '</i>' . ' ' . $v . '; ';
-                            echo $result;
-                            // $name = get_sub_field('name') ? ' <i>' . get_sub_field('name') . '</i>' : '';
-                            // $designation = ' ' . get_sub_field('designation') . '; ';
+                        // $name_array[] = get_sub_field('name');
+                        // $designation_array[] = get_sub_field('designation');
+                        // $combine_arrays = array_combine($name_array, $designation_array);
+                        // foreach($combine_arrays as $k => $v) {
+                        //     $v_check = $v ? ' ' . $v : '';
+                        //     $data[] = ' <i>' . $k . '</i>' . $v_check;
+                        // }
+                        foreach ($plant_names as $name) {
+                            $name = get_sub_field('name') ? ' <i>' . get_sub_field('name') . '</i>' : '';
+                            $designation = ' ' . get_sub_field('designation') . '; ';
                             // $array = ' <i>' . implode(' ', $name_array) . '</i> ' . implode(" ", $designation_array);
                         }
                     }
+                    echo $name, $designation;
                 }
             }
-            // $combine_arrays = array_combine($name_array, $designation_array);
-            // foreach($combine_arrays as $k => $v) {
-            //     $v_check = $v ? ' ' . $v : '';
-            // }
-            // $data[] = ' <i>' . $k . '</i>' . $v_check;
-            // print_r($data);
     }
-    echo 
+
+    echo
         '</h2>
     </div>';
 }
