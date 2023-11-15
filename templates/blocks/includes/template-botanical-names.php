b0075414fb5a3f9a1a5a179e7197b803c02dc420
MattKCyr <matt@mspacecreative.com>
2023-11-15 10:18:31 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,40 +1,35 @@
 <?php
 if (have_rows('botanical_names', get_the_ID($term_id))) {
-
-    $names = get_row_index();
-    $check_plural = $names > 1 ? 's' : '';
-
-    echo
-    '<div class="botanical-names">
-        <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     
     while (have_rows('botanical_names', get_the_ID($term_id))) {
         the_row();
 
-        $names = get_row_index();
+        $name_count = get_row_index();
+        $check_plural = $name_count > 1 ? 's' : '';
+
+        echo
+        '<div class="botanical-names">
+            <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
             if (have_rows('botanical_name')) {
                 while (have_rows('botanical_name')) {
                     the_row();
 
-                    print_r($names);
-
                     if ( $plant_names = get_row() ) {
-                        $many_names = $names > 1 ? '; ' : '';
+                        $many_names = $name_count > 1 ? '; ' : '';
                         foreach ($plant_names as $plant_name) {
                             $name = get_sub_field('name', $plant_name) ? ' <i>' . get_sub_field('name', $plant_name) . '</i>' : '';
                             $designation = ' ' . get_sub_field('designation', $plant_name) . $many_names;
                         }
                     }
-                    // print_r($count);
                     echo $name, $designation;
                 }
             }
+            
+            echo
+            '</h2>
+        </div>';
     }
-
-    echo
-        '</h2>
-    </div>';
 }
 // foreach($combine as $k => $v) {
 //     $v_check = $v ? ' ' . $v : '';
