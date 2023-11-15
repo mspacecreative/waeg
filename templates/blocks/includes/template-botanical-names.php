f28c5f438989d337f49e8a20ba9053b3560f4da9
MattKCyr <matt@mspacecreative.com>
2023-11-15 16:04:21 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -8,23 +8,19 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
     '<div class="botanical-names">
         <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     
-    while (have_rows('botanical_names', get_the_ID($term_id))) {
-        the_row();
+        if (have_rows('botanical_name')) {
+            while (have_rows('botanical_name')) {
+                the_row();
+                $name_array[] = get_sub_field('name');
+                $designation_array[] = get_sub_field('designation');
+                $combined_array = array_combine($name_array, $designation_array);
 
-            if (have_rows('botanical_name')) {
-                while (have_rows('botanical_name')) {
-                    the_row();
-                    $name_array[] = get_sub_field('name');
-                    $designation_array[] = get_sub_field('designation');
-                    $combined_array = array_combine($name_array, $designation_array);
-
-                    foreach($combined_array as $k => $v) {
-                        $v_check = $v ? ' ' . $v : '';
-                    }
+                foreach($combined_array as $k => $v) {
+                    $v_check = $v ? ' ' . $v : '';
                 }
             }
-            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
-    }
+        }
+        $names_array[] = ' <i>' . $k . '</i>' . $v_check;
     echo implode('; ', $names_array);
 
     echo
