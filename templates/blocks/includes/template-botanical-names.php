49683018ab8f2b07822c81651b94f74cf929670c
MattKCyr <matt@mspacecreative.com>
2023-11-15 09:13:01 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -21,17 +21,18 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
                         $name_array[] = get_sub_field('name');
                         $designation_array[] = get_sub_field('designation');
                         $combine_arrays = array_combine($name_array, $designation_array);
-                        foreach($combine_arrays as $k => $v) {
-                            $v_check = $v ? ' ' . $v : '';
-                            $data[] = ' <i>' . $k . '</i>' . $v_check;
-                            echo implode('; ', $data);
-                        }
                         // foreach ($plant_names as $name) {
                         //     $name = get_sub_field('name') ? ' <i>' . get_sub_field('name') . '</i>' : '';
                         //     $designation = ' ' . get_sub_field('designation') . '; ';
                         //     // $array = ' <i>' . implode(' ', $name_array) . '</i> ' . implode(" ", $designation_array);
                         // }
                     }
+                    foreach($combine_arrays as $k => $v) {
+                        $v_check = $v ? ' ' . $v : '';
+                        $data[] = ' <i>' . $k . '</i>' . $v_check;
+                    }
+                    
+                    echo implode('; ', $data);
                     // $semicolon = 
                     // echo $name, $designation;
                 }
