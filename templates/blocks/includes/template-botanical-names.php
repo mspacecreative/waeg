7c40f829fe405c50c5fbfc099cd5ad6588cbb48b
MattKCyr <matt@mspacecreative.com>
2023-11-15 16:07:36 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,5 +1,5 @@
 <?php
-if (have_rows('botanical_names', get_the_ID($term_id))) {
+// if (have_rows('botanical_names', get_the_ID($term_id))) {
 
     $name_count = count(get_field('botanical_names', get_the_ID($term_id)));
     $check_plural = $name_count > 1 ? 's' : '';
@@ -8,24 +8,27 @@ if (have_rows('botanical_names', get_the_ID($term_id))) {
     '<div class="botanical-names">
         <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
     
-        if (have_rows('botanical_name', get_the_ID($term_id))) {
-            while (have_rows('botanical_name', get_the_ID($term_id))) {
-                the_row();
-                $name_array[] = get_sub_field('name', get_the_ID($term_id));
-                $designation_array[] = get_sub_field('designation', get_the_ID($term_id));
-                $combined_array = array_combine($name_array, $designation_array);
+    while (have_rows('botanical_names', get_the_ID($term_id))) {
+        the_row();
 
-                foreach($combined_array as $k => $v) {
-                    $v_check = $v ? ' ' . $v : '';
+            if (have_rows('botanical_name')) {
+                while (have_rows('botanical_name')) {
+                    the_row();
+                    $name_array[] = get_sub_field('name');
+                    $designation_array[] = get_sub_field('designation');
+                    $combined_array = array_combine($name_array, $designation_array);
+
+                    foreach($combined_array as $k => $v) {
+                        $v_check = $v ? ' ' . $v : '';
+                    }
                 }
             }
-        }
-
-        $names_array[] = ' <i>' . $k . '</i>' . $v_check;
+            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
+    }
     echo implode('; ', $names_array);
 
     echo
         '</h2>
     </div>';
-}
+// }
 
