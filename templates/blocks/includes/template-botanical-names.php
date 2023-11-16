c260d804d2815693a8ea081e5aa7f9cb8037f702
MattKCyr <matt@mspacecreative.com>
2023-11-16 15:41:55 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -6,14 +6,13 @@ echo
 '<div class="botanical-names">
     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
-while (have_rows('botanical_names', get_the_ID())) {
+while (have_rows('botanical_names', get_the_ID($term_id))) {
     the_row();
 
-        while (have_rows('botanical_name', get_the_ID())) {
+        while (have_rows('botanical_name')) {
             the_row();
-            $row = get_row();
-            $name_array[] = $row['name'];
-            $designation_array[] = $row['designation'];
+            $name_array[] = get_sub_field('name');
+            $designation_array[] = get_sub_field('designation');
             $combined_array = array_combine($name_array, $designation_array);
 
             foreach($combined_array as $k => $v) {
