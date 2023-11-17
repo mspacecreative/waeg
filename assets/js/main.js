a74033bb61f3829dbd713ad67209584eab2c0bbb
MattKCyr <matt@mspacecreative.com>
2023-11-17 09:54:58 -0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,89 @@
+// SLIDE TOGGLE
+
+let slideUp = (target, duration=500) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.boxSizing = 'border-box';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout( () => {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    //alert("!");
+  }, duration);
+}
+
+let slideDown = (target, duration=500) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+
+  if (display === 'none')
+    display = 'block';
+
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout( () => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+}
+var slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
+
+// document.getElementById("triggerUp").addEventListener('click', function() {
+//   slideUp(document.getElementById("target"), 200);
+// });
+
+// document.getElementById("triggerDown").addEventListener('click', function() {
+//   slideDown(document.getElementById("target"), 200);
+// });
+
+document.getElementById("triggerToggle").addEventListener('click', function() {
+  slideToggle(document.getElementById("target"), 200);
+});
+
 // VIRTUAL TOUR NAVIGATION
 const navToggle = document.querySelector('.virtual-tour-navigation__toggle');
 const virtualTourNav = document.querySelector('nav.virtual-tour__navigation');
 
 navToggle.addEventListener('click', function() {
-  virtualTourNav.classList.toggle('show');
+  // virtualTourNav.classList.toggle('show');
+  slideToggle(virtualTourNav, 200);
   if (this.innerHTML === "NAVIGATION") {
     this.innerHTML="CLOSE";
   } else {
