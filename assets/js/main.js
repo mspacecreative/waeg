017d814dd12b6a6bb37b3dd460c0e7882bb0e602
MattKCyr <matt@mspacecreative.com>
2023-11-17 10:47:45 -0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,10 +165,13 @@ window.addEventListener("resize", () => {
   if (!document.body.classList.contains("home")) {
     mainTag.style.marginTop = headerHeight.clientHeight + "px";
   }
-  if (window.matchMedia("(min-width: 1150px)").matches) {
+  // if (window.matchMedia("(min-width: 1150px)").matches) {
+  //   virtualTourNav.style.display = 'block';
+  // } else {
+  //   virtualTourNav.style.display = 'none';
+  // }
+  if (navToggle.style.display === 'none') {
     virtualTourNav.style.display = 'block';
-  } else {
-    virtualTourNav.style.display = 'none';
   }
 });
 
