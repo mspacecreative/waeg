1e71ccc67c23f9211a12f351977980b50885625b
MattKCyr <matt@mspacecreative.com>
2023-11-17 16:14:56 -0400

    C

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -6,7 +6,7 @@ echo
 '<div class="botanical-names">
     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
-while (have_rows('botanical_names', get_the_ID($query->ID))) {
+while (have_rows('botanical_names', get_the_ID())) {
     the_row();
 
         while (have_rows('botanical_name')) {
