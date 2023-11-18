0f4ee804ca51c047a134143b6b94302ca0013579
MattKCyr <matt@mspacecreative.com>
2023-11-18 13:08:14 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -9,7 +9,7 @@ echo
 while (have_rows('botanical_names', get_the_ID($term_id))) {
     the_row();
 
-    $name_rows = get_row(get_the_ID($query->ID));
+    $name_rows = get_row(get_the_ID());
 
     foreach ($name_rows as $name_row) {
         $names = $name_row['name'];
@@ -19,8 +19,8 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
         $combined_array = array_combine($name_array, $designation_array);
 
         foreach($combined_array as $k => $v)
+            $v_check = $v ? ' ' . $v : '';
     }
-    $v_check = $v ? ' ' . $v : '';
     $names_array[] = ' <i>' . $k . '</i>' . $v_check;
 }      
         echo implode('; ', $names_array);
