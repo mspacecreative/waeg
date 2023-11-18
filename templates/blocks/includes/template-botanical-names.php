9f9140d84629110539585018a4b5c7c26e0e59d0
MattKCyr <matt@mspacecreative.com>
2023-11-18 12:45:50 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -9,16 +9,21 @@ echo
 while (have_rows('botanical_names', get_the_ID($term_id))) {
     the_row();
 
-    while (have_rows('botanical_name')) {
-        the_row();
-        $name_array[] = get_sub_field('name');
-        $designation_array[] = get_sub_field('designation');
-        $combined_array = array_combine($name_array, $designation_array);
+    $name_rows = get_row();
 
-        foreach($combined_array as $k => $v)
-            $v_check = $v ? ' ' . $v : '';
-            
-        $names_array[] = ' <i>' . $k . '</i>' . $v_check;
+    foreach ($name_rows as $name_row) {
+
+        while (have_rows('botanical_name')) {
+            the_row();
+            $name_array[] = get_sub_field('name');
+            $designation_array[] = get_sub_field('designation');
+            $combined_array = array_combine($name_array, $designation_array);
+
+            foreach($combined_array as $k => $v)
+                $v_check = $v ? ' ' . $v : '';
+                
+            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
+        }
     }
 }  
         print_r($combined_array);
