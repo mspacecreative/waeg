3f5973447109500b23ceef49a7f8aa42dc2880ac
MattKCyr <matt@mspacecreative.com>
2023-11-18 13:10:23 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -9,7 +9,7 @@ echo
 while (have_rows('botanical_names', get_the_ID())) {
     the_row();
 
-    $name_rows = get_row(get_the_ID());
+    $name_rows = get_row($query->ID);
 
     foreach ($name_rows as $name_row) {
         $names = $name_row['name'];
