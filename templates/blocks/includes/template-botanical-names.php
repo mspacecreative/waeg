249a96e9fed01f67aae7d5ae30e282d953bedc78
MattKCyr <matt@mspacecreative.com>
2023-11-18 14:24:31 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -5,7 +5,7 @@ $check_plural = $name_count > 1 ? 's' : '';
 echo
 '<div class="botanical-names">
     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
-
+reset_rows('botanical_names', get_the_ID($term_id)));
 while (have_rows('botanical_names', get_the_ID($term_id))) {
     the_row();
 
