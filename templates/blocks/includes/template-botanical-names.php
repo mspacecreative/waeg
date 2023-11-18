527362cd934660860af54f30223c0f82aa7bcf58
MattKCyr <matt@mspacecreative.com>
2023-11-18 13:11:31 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -10,19 +10,18 @@ while (have_rows('botanical_names', get_the_ID())) {
     the_row();
 
     $name_rows = get_row(get_the_ID($query->ID));
+}
+foreach ($name_rows as $name_row) {
+    $names = $name_row['name'];
+    $designations = $name_row['designation'];
+    $name_array[] = $names;
+    $designation_array[] = $designations;
+    $combined_array = array_combine($name_array, $designation_array);
 
-    foreach ($name_rows as $name_row) {
-        $names = $name_row['name'];
-        $designations = $name_row['designation'];
-        $name_array[] = $names;
-        $designation_array[] = $designations;
-        $combined_array = array_combine($name_array, $designation_array);
-
-        foreach($combined_array as $k => $v)
-            $v_check = $v ? ' ' . $v : '';
-    }
-    $names_array[] = ' <i>' . $k . '</i>' . $v_check;
-}      
+    foreach($combined_array as $k => $v)
+        $v_check = $v ? ' ' . $v : '';
+}
+$names_array[] = ' <i>' . $k . '</i>' . $v_check;
         echo implode('; ', $names_array);
         // print_r($names_array);
 
