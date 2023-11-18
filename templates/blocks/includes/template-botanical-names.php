e2e24b83d24797f7d767ff2c2cbfe5806f03a073
MattKCyr <matt@mspacecreative.com>
2023-11-18 14:17:27 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -1,35 +1,36 @@
 <?php
-$repeater_field = get_field('botanical_names', get_the_ID($term_id));
-$name_count = count($repeater_field);
+$name_count = count(get_field('botanical_names', get_the_ID($term_id)));
 $check_plural = $name_count > 1 ? 's' : '';
 
 echo
 '<div class="botanical-names">
     <h2 class="has-medium-font-size"><strong>' . esc_html_x('Botanical name', 'waeg') . $check_plural . ':</strong>';
 
-// while (have_rows('botanical_names', get_the_ID($term_id))) {
-//     the_row();
+foreach ($query->posts as $nested_post) {
 
-    // while (have_rows('botanical_name', get_the_ID())) {
-    //     the_row();
+    while (have_rows('botanical_names', get_the_ID($nested_post))) {
+    the_row();
 
-        $name_rows = $repeater_field;
+    while (have_rows('botanical_name', get_the_ID())) {
+        the_row();
+
+        $name_rows = get_row(get_the_ID($nested_post));
 
         foreach ($name_rows as $name_row) {
-            setup_postdata($name_row);
-            $names = get_sub_field('name', get_the_ID($name_row));
-            $designations = get_sub_field('designation', get_the_ID($name_row));
+            $names = get_sub_field('name');
+            $designations = get_sub_field('designation');
+            $name_array[] = $names;
+            $designation_array[] = $designations;
         }
-    // }
-    $name_array[] = $names;
-    $designation_array[] = $designations;
+    }
     $combined_array = array_combine($name_array, $designation_array);
 
     foreach($combined_array as $k => $v)
         $v_check = $v ? ' ' . $v : '';
     
     $names_array[] = ' <i>' . $k . '</i>' . $v_check;
-// }      
+    }      
+}
         echo implode('; ', $names_array);
 echo
     '</h2>
