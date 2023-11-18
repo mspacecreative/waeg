210ea3a48a7ad864d81352fa0e4992b5f6ed08b2
MattKCyr <matt@mspacecreative.com>
2023-11-18 14:01:41 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -18,16 +18,15 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
             $names = get_sub_field('name');
             $designations = get_sub_field('designation');
         }
-        
-        $name_array[] = $names;
-        $designation_array[] = $designations;
-        $combined_array = array_combine($name_array, $designation_array);
-
-        foreach($combined_array as $k => $v)
-            $v_check = $v ? ' ' . $v : '';
-        
-        $names_array[] = ' <i>' . $k . '</i>' . $v_check;
     }
+    $name_array[] = $names;
+    $designation_array[] = $designations;
+    $combined_array = array_combine($name_array, $designation_array);
+
+    foreach($combined_array as $k => $v)
+        $v_check = $v ? ' ' . $v : '';
+    
+    $names_array[] = ' <i>' . $k . '</i>' . $v_check;
 }      
         echo implode('; ', $names_array);
         // print_r($names_array);
