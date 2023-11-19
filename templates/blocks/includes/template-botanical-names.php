24469312c1d0e60430ade9f037868cfbdc8ce9a1
MattKCyr <matt@mspacecreative.com>
2023-11-19 15:39:32 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -22,7 +22,7 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
 
         foreach($combined_array as $k => $v)
             $v_check = $v ? ' ' . $v : '';
-            $names_array = ' <i>' . $k . '</i>' . $v_check;
+            $names_array[] = ' <i>' . $k . '</i>' . $v_check;
         // $names_array = [];   
         // $names_array = ' <i>' . $k . '</i>' . $v_check;
         // $names_array = explode(", ", $names_array);
@@ -34,7 +34,6 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
         // $merged_array = array_merge($combined_array);
         // print_r($merged_array);
     }
-    $names_array = [];
     $names_array = explode(", ", $names_array);
     $r = array_merge(...$names_array);
     $flat = call_user_func_array('array_merge', array($r));
