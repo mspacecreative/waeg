6b61dd9d36d203152c1f0d3ca33a43b81729f6f7
MattKCyr <matt@mspacecreative.com>
2023-11-19 20:53:02 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -38,8 +38,8 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
     // $r = array_merge(...array($names_array));
     // $flat = call_user_func_array('array_merge', array($r));
 // $combine_arrays = array_merge(...$names_array);
-    // print_r($r);
-    echo implode("; ", $combined_array);
+    print_r($combined_array);
+    // echo implode("; ", $combined_array);
 }
 
 echo
