b1e8cf98b68dbe0e3972857f007fba68981ea4bd
MattKCyr <matt@mspacecreative.com>
2023-11-19 21:25:22 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -19,9 +19,6 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
         $designations_array = [];
         $designations_array = explode(", ", $designations);
         $combined_array = array_combine($names_array, $designations_array);
-        $new_array = array();
-        foreach($combined_array as $k => $v)
-            $v_check = $v ? ' ' . $v : '';
             
         // $names_array = [];   
         // $names_array = ' <i>' . $k . '</i>' . $v_check;
@@ -34,10 +31,11 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
         // $merged_array = array_merge($combined_array);
         // print_r($merged_array);
     }
+    foreach($combined_array as $k => $v)
+        $v_check = $v ? ' ' . $v : '';
     $new_array = ' <i>' . $k . '</i>' . $v_check;
-    $new_array = array_merge(array($new_array), array($v_check));
     // $new_array = [];
-    // $new_array = explode(", ", $new_array);
+    $new_array = explode(", ", $new_array);
     // $r = array_merge(...array($names_array));
     // $flat = call_user_func_array('array_merge', array($r));
 // $combine_arrays = array_merge(...$names_array);
