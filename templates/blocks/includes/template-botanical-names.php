6f201034e42d824714d7e88ba8b2a1b84cbc51ef
MattKCyr <matt@mspacecreative.com>
2023-11-20 09:08:25 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -11,20 +11,17 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
 
     while (have_rows('botanical_name', get_the_ID($term_id))) {
         the_row();
-        // $row = get_row(get_the_ID());
-        // $names = array();
-        $names[] = get_sub_field('name');
-        // $designations = array();
-        $designations[] = get_sub_field('designation', get_the_ID($term_id));
-        // $names_array = [];
-        // $names_array = explode(", ", $names);
-        // $designations_array = [];
-        // $designations_array = explode(", ", $designations);
-        // $combined_array = array_combine($names_array, $designations_array);
+        $names = get_sub_field('name', get_the_ID($term_id));
+        $names_array = [];
+        $names_array = explode(", ", $names);
+        $designations = get_sub_field('designation', get_the_ID($term_id));
+        $designations_array = [];
+        $designations_array = explode(", ", $designations);
+        $combined_array = array_combine($names_array, $designations_array);
+        foreach($names_array as $k => $v)
+            $v_check = $v ? ' ' . $v : '';
         // $designations_array = array_merge(...array($designations_array));
-            
-        // $names_array = [];   
-        // $names_array = ' <i>' . $k . '</i>' . $v_check;
+        print_r($names_array);
         // $names_array = explode(", ", $names_array);
         // // $names_array = array_merge(...$names_array);
         // print_r($names_array);
@@ -34,7 +31,6 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
         // $merged_array = array_merge($combined_array);
         // print_r($merged_array);
     }
-    print_r($names);
     // foreach($combined_array as $k => $v)
     //     $v_check = $v ? ' ' . $v : '';
     // $combined_array = ' <i>' . $k . '</i>' . $v_check;
