e98bd8fd0953a8047a9ca91ca5467c4929db85b2
MattKCyr <matt@mspacecreative.com>
2023-11-20 15:33:56 -0400

    C

--- templates/blocks/block-anchor-link.php
+++ templates/blocks/block-anchor-link.php
@@ -14,7 +14,7 @@ $loop = new WP_Query( array(
 echo 
 '<a class="anchor-link__button" href="' . home_url('virtual-tour') . '#';
 while ( $loop->have_posts() ) : $loop->the_post();
-    $title = sanitize_title(get_the_title());
+    $title = sanitize_title(get_the_title($loop->ID));
     echo $title;
 endwhile;
 echo
