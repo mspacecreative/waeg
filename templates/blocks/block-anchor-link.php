4b61d5bc989f6815348bef58fcc172a35fa3d159
MattKCyr <matt@mspacecreative.com>
2023-11-20 15:42:55 -0400

    c

--- templates/blocks/block-anchor-link.php
+++ templates/blocks/block-anchor-link.php
@@ -1,6 +1,24 @@
 <?php
+$term_id = get_term(get_the_ID(), 'species');
+$term_slug = $term_id->slug;
+switch($slug) {
+    case 'flowers-and-herbs':
+        $slug = 'flowers-and-herbs';
+        break;
+    case 'shrubs':
+        $slug = 'shrubs';
+        break;
+    case 'trees':
+        $slug = 'trees';
+        break;
+    case 'other-important-plants-and-species':
+        $slug = 'other-important-plants-and-species';
+        break;
+    default:
+        $slug = '';
+}
 echo 
-'<a class="anchor-link__button" href="' . home_url('virtual-tour') . '#';
+'<a class="anchor-link__button" href="' . home_url('virtual-tour') . $slug . '#';
     $title = sanitize_title(get_the_title());
     echo $title;
 echo
