17f3951ac5324408c7d78fcc5787ee8ef379b6bf
MattKCyr <matt@mspacecreative.com>
2023-11-21 08:38:05 -0400

    c

--- templates/blocks/includes/template-botanical-names.php
+++ templates/blocks/includes/template-botanical-names.php
@@ -21,13 +21,20 @@ while (have_rows('botanical_names', get_the_ID($term_id))) {
     
     $data = '';
 
-    foreach($combined_array as $k => $v) {
-        $v_check = $v ? ' ' . $v . ',' : ',';
-        $data = " <i>$k</i>$v_check";
-    }
-    $data = explode(",", $data);
-    echo implode(";", $data);
+    // foreach($combined_array as $k => $v) {
+    //     $v_check = $v ? ' ' . $v . ',' : ',';
+    //     $data = " <i>$k</i>$v_check";
+    // }
+    // $data = explode(",", $data);
+    // echo implode(";", $data);
 }
+
+foreach($combined_array as $k => $v) {
+    $v_check = $v ? ' ' . $v . ',' : ',';
+    $data = " <i>$k</i>$v_check";
+}
+$data = explode(",", $data);
+        echo implode(";", $data);
 
 echo
     '</h2>
