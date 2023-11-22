9bf4071a2bc6a1634cddbc3f6926d15092b4ae90
MattKCyr <matt@mspacecreative.com>
2023-11-22 11:10:15 -0400

    c

--- functions/patterns/post-modal-loop.php
+++ functions/patterns/post-modal-loop.php
@@ -1,25 +1,2 @@
 <?php
-$id_counter = 1;
-$args = array(
-    'post_type' => 'plant',
-    'posts_per_page' => -1,
-    'orderby' => 'name',
-    'order' => 'ASC',
-    'tax_query' => array(
-        array(
-            'taxonomy' => 'species',
-            'field' => 'term_id',
-            'terms' => 'berries'
-        )
-    )
-
-);
-
-$query = new WP_Query($args);
-
-while ($query->have_posts()) {
-    $query->the_post();
-    $title = get_the_title($query->ID);
-    echo
-    '<h1 id="post-title-' . $id_counter++ . '" class="modal-title has-large-font-size" style="margin: 0;">' . $title . '</h1>';
-} wp_reset_postdata();
+echo 'hello world';
