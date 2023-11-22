a5a618614733388daf0909c9c4085eddfe0ae55d
MattKCyr <matt@mspacecreative.com>
2023-11-22 10:49:48 -0400

    c

--- functions/post-modal.php
+++ functions/post-modal.php
@@ -1,8 +1,8 @@
 <?php
 function post_modal() {
-    // ob_start();
-    // include 'patterns/post-modal.php';
-    // ob_clean();
+    ob_start();
+    get_template_part('patterns/post-modal');
+    ob_clean();
     echo 'stuff';
 }
 add_action( 'wp_footer', 'post_modal', 1 );
