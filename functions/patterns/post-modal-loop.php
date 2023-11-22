c20fdb94953f6b2bb15ed0e03384b3f34722accf
MattKCyr <matt@mspacecreative.com>
2023-11-22 11:09:36 -0400

    c

--- functions/patterns/post-modal-loop.php
+++ functions/patterns/post-modal-loop.php
@@ -17,11 +17,9 @@ $args = array(
 
 $query = new WP_Query($args);
 
-if ($query->have_posts()) {
-    while ($query->have_posts()) {
+while ($query->have_posts()) {
     $query->the_post();
     $title = get_the_title($query->ID);
     echo
     '<h1 id="post-title-' . $id_counter++ . '" class="modal-title has-large-font-size" style="margin: 0;">' . $title . '</h1>';
-    }
 } wp_reset_postdata();
