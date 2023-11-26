937b6e7fb6f19cfe4b5894124cbaa7a3a542fe5d
MattKCyr <matt@mspacecreative.com>
2023-11-26 12:55:26 -0400

    c

--- functions/post-modal.php
+++ functions/post-modal.php
@@ -1,6 +1,5 @@
 <?php
 function post_modal() {
-    // include 'patterns/post-modal-loop.php';
-    echo 'test';
+    include 'patterns/post-modal-loop.php';
 }
 add_action( 'wp_head', 'post_modal' );
