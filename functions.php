8d93343df8eed3e2e435cd7b353f9f8006b49e30
MattKCyr <matt@mspacecreative.com>
2023-11-27 10:39:20 -0400

    c

--- functions.php
+++ functions.php
@@ -28,4 +28,3 @@ include 'functions/audio-loop.php';
 include 'functions/body-class.php';
 include 'functions/admin-styles.php';
 include 'functions/excerpt-to-post-type.php';
-include 'functions/post-modal.php';
