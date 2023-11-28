cd1f6913ba3488a8cb30995fe413b3bdd6761287
MattKCyr <matt@mspacecreative.com>
2023-11-28 12:47:11 -0400

    c

--- templates/blocks/block-anchor-link.php
+++ templates/blocks/block-anchor-link.php
@@ -2,7 +2,7 @@
 $term_id = get_the_terms(get_the_ID(), 'species');
 $term_slug = array_shift($term_id);
 $slug = $term_slug->slug;
-$language_check = ICL_LANGUAGE_CODE == 'fr' ? 'visite-virtuelle' . '/' . $slug : 'virtual-tour' . '/' . $slug;
+$language_check = ICL_LANGUAGE_CODE == 'fr' && $slug !== 'baies' ? 'visite-virtuelle' . '/' . $slug : $slug !== 'berries' ? 'virtual-tour' . '/' . $slug : 'virtual-tour' . '/';
 switch($slug) {
     case 'flowering-plants':
         $slug = 'flowering-plants';
